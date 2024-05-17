c85a21894109632b2f7f117fe970967437ed9f53
amitfxcareers <amit@fxcareers.com>
2024-05-17 15:08:57 +0530

    menu added

---
+++ include/footer.php
@@ -0,0 +1,374 @@
+<section class="footer-section px-0">
+  <div class="container">
+    <div class="row g-4">
+      <div class="col-md-4">
+        <img src="images/nxgmarkets.webp" class='footer-logo' height="80px" />
+        <div class="social-media-icon">
+          <a href="#"> <i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+          <a href="#"> <i class="fa-brands fa-linkedin"></i></a>
+          <a href="#"> <i class="fa-brands fa-instagram"></i></a>
+          <a href="#"> <i class="fa-brands fa-pinterest"></i></a>
+          <a href="#"> <i class="fa-brands fa-youtube"></i></a>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="col-width">
+          <h6>About company</h6>
+          <ul>
+            <li>
+              <a href="about-us.php">
+                About us
+              </a>
+            </li>
+            <li>
+              <a href="why-choose-us.php">
+                Why choose us
+              </a>
+            </li>
+            <li>
+              <a href="blog.php">
+                Our Blog
+              </a>
+            </li>
+            <li>
+              <a href="careers.php">
+                Careers
+              </a>
+            </li>
+            <!-- <li>
+              <a href="our-team.php">
+                Meet our Team
+              </a>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="col-width">
+          <h6>Useful Link</h6>
+          <ul>
+            <li>
+              <a href="broker-setup.php">
+                Broker Setup
+              </a>
+            </li>
+            <li>
+              <a href="liquidity.php">
+                Liquidity Solutions
+              </a>
+            </li>
+            <li>
+              <a href="institutional-clients.php">
+                Institutional Clients
+              </a>
+            </li>
+            <li>
+              <a href="metatrader-support.php">
+                Metatrader Support
+              </a>
+            </li>
+            <!-- <li>
+              <a href="portfolio.php">
+                Portfolio
+              </a>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <h4 class="text-primary">Connect with us</h4>
+        <div class="address">
+
+
+          <p><i class="fa-solid fa-location-dot"></i> Suite 305, Griffith Corporate Centre, Kingstown P.O. Box
+            1510, Beachmont Kingstown St. Vincent and the Grenadines</p>
+
+          <p><i class="fa-solid fa-phone"></i> +357 22030234</p>
+
+          <p> <i class="fa-solid fa-envelope"></i> support@yaprime.com</p>
+        </div>
+      </div>
+
+    </div>
+
+    <hr class="footer_hr" />
+    <p>
+    	NXG Markets Pty Ltd, an ASIC regulated for the general financial product advice (ABN 93 623 948 998, AFS License No. 001308207)
+		NXG Markets Limited is regulated by the MISA (MWALI International Services Authority) for the brokerage services (<a href="https://mwaliregistrar.com/list_of_entities/authorised_brokerage_companies">License T2022091</a>).<br>
+		<b>Physical Address</b>: #806, AL SERKAL BUILDING 2 PORT SAEED DUBAI U.A.E.<br>
+		<b>Registered Address</b>: BONOVO ROAD – FOMBONI ISLAND OF MOHELI – COMOROS UNION
+    </p>
+    <p>
+    	<b>RISK WARNING</b>
+    </p>
+    <p>
+    	<b>Risk statement</b>: An investment in derivatives may mean investors may lose an amount even greater than their original investment. Anyone wishing to invest in any of the products mentioned here should seek their own financial or professional advice. Trading of securities, forex, stock market, commodities, options and futures may not be suitable for everyone and involves the risk of losing part or all of your money. Trading in the financial markets has large potential rewards, but also large potential risk. You must be aware of the risks and be willing to accept them in order to invest in the markets. Don’t invest and trade with money which you can’t afford to lose. Forex Trading are not allowed in some countries, before investing your money, make sure whether your country is allowing this or not. You are strongly advised to obtain independent financial, legal and tax advice before proceeding with any currency or spot metals trade. Nothing in this site should be read or construed as constituting advice on the part of NXG Markets Limited or any of its affiliates, directors, officers or employees.
+    </p>
+    <p>
+    	<b>Restricted Regions</b>: NXG Markets Limited does not provide services for citizens/residents of the United States, Cuba, Iraq, Myanmar, North Korea & Sudan. The services of NXG Markets Limited are not intended for distribution to, or use by, any person in any country or jurisdiction where such distribution or use would be contrary to local law or regulation. <a href="risk-disclosure.php">Risk disclosure</a>
+    </p>
+    <hr class="footer_hr" />
+    <div class="row">
+      <div class="col-md-6">
+        <div class="copyrights">
+          <p class="mb-3">© 2024 <a href="https://www.nxgmarkets.com/">NXG Markets</a>. All Rights Reserved.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="copyrights">
+          <p><a href="disclaimer.php">Disclaimer</a> </p>
+          <p><a href="terms-conditions.php">Terms & Conditions</a></p>
+          <p><a href="privacy-policy.php">Privacy Policy</a></p>
+          <p><a href="refund-cancellation.php">Refund & Cancellation</a></p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Create Your Prime Account </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="container">
+          <div class="row g-4">
+            <div class="col-lg-6 align-self-center">
+              <img src="images/requst-quote.png" class="img-fluid">
+            </div>
+            <div class="col-lg-6 pop_up_form_class">
+              <form>
+                <div class="row g-4 form-group mb-3">
+                  <div class="col-lg-6 col-md-12">
+                    <label for="formGroupExampleInput">Full Name</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name" required="" name="name" data-gtm-form-interact-field-id="1">
+                  </div>
+                  <div class=" col-lg-6  col-md-12">
+                    <label for="formGroupExampleInput2">Email Address</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email Address" required="" name="email" data-gtm-form-interact-field-id="2">
+                  </div>
+                </div>
+                <div class="row g-4 form-group mb-3">
+                  <div class="col-lg-6  col-md-12">
+                    <label for="inputState">Contact Number</label>
+                    <input type="text" placeholder="Enter Contact Number" name="contact_number" class="form-control">
+                  </div>
+                  <div class=" col-lg-6 col-md-12">
+                    <label for="inputState">Your Country</label>
+                    <input type="text" placeholder="Your Country" name="country" class="form-control">
+                  </div>
+                </div>
+                <div class="row g-4 form-group mb-3">
+                  <div class="col-md-12">
+                    <label for="inputState">Are You A ?</label>
+                    <select class="form-control" id="areyoua" name="areyoua" required="">
+                      <option value="">Select</option>
+                      <option value="Start Up">Start Up</option>
+                      <option value="Introducing Broker">Introducing Broker</option>
+                      <option value="Existing Broker">Existing Broker</option>
+                      <option value="Others">Others</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="row g-4 form-group mb-3">
+                  <div class="col-md-12">
+                    <label for="inputState">What are the product or services you looking for?</label>
+                    <select class="form-control" id="products" name="products" required="">
+                      <option disabled="" value="" selected="">Select Products</option>
+                      <option value="Traders Room">Traders Room</option>
+                      <option value="Forex Brokerage">Forex Brokerage</option>
+                      <option value="MT5 White Label">MT5 White Label</option>
+                      <option value="MT5 ">MT5 </option>
+                      <option value="Forex Websites Broker">Forex Websites Broker</option>
+                      <option value="Forex CRM">Forex CRM</option>
+                      <option value="Forex Liquidity">Forex Liquidity</option>
+                      <option value="MAM">MAM</option>
+                      <option value="MAM/PAMM Support">MAM/PAMM Support</option>
+                      <option value="PAMM">PAMM</option>
+                      <option value="Merchant Services">Merchant Services</option>
+                      <option value="Deposits And Withdrawals With Bitcoin-ETH-USDT">Deposits And Withdrawals
+                        With
+                        Bitcoin-ETH-USDT</option>
+                      <option value="IB &amp; Affiliate Area">IB &amp; Affiliate Area</option>
+                      <option value="All The Above">All The Above</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="row g-4 form-group mb-3">
+                  <div class="col-md-12">
+                    <label for="inputState">Do you need a quote ?</label>
+                    <select class="form-control" id="qoute" name="qoute" required="">
+                      <option value="">Select</option>
+                      <option value="Yes">Yes</option>
+                      <option value="No">No</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group mb-3">
+                  <label for="exampleFormControlTextarea1">Give me a brief about your requirement</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="1" cols="50" placeholder="Message" name="message"></textarea>
+                </div>
+                <button type="submit" class="unique_anchor_one btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- <script src="./vendor/font-awesome/js/all.min.js"></script> -->
+<script src="./vendor/owl-carousel/js/owl.carousel.js"></script>
+<script type="text/javascript" src="./js/youtube-popup.js"></script>
+<script src="./js/app.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+<script>
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("StickyHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+</script>
+<script type="text/javascript">
+  (function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout:3000,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+</script>
+
+<script type="text/javascript">
+  (function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel11").owlCarousel({
+      loop: true,
+      margin: 30,
+      responsiveClass: true,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout:3000,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 2,
+          nav: false
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+</script>
+
+<script type="text/javascript">
+  (function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel111").owlCarousel({
+      loop: true,
+      margin: 30,
+      responsiveClass: true,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout:3000,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 4,
+          nav: false
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+</script>
+
+<script>
+    // mobile menu dropdows
+    $(".moblie-dropdwon").on("click", function () {
+        var current_dropdown = $(this).next(".dropdown-mb-menu");
+        $(".dropdown-mb-menu").not(current_dropdown).slideUp();
+        current_dropdown.slideToggle();
+        return false;
+    });
+    
+</script>
+</body>
+
+</html>
+
+<script src="//code.jivosite.com/widget/4QCPKiN4Lm" async></script>
