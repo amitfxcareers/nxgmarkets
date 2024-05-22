b63257d490f267a2804c8a600865f16cfe041e88
amitfxcareers <amit@fxcareers.com>
2024-05-22 10:09:13 +0530

    why trade section done

---
+++ include/trade.php
@@ -0,0 +1,58 @@
+<section class="trade-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="main-heading">Trade Forex</h2>
+				<p class="text-center">
+					Forex is the worldwide marketplace for currency exchange, providing traders with opportunities to profit from fluctuations in exchange rates. With a daily trade volume of $6.5 trillion, Forex offers substantial potential for both novice and professional traders. Open your trading account with NXG Markets and trade the forex markets securely.
+				</p>
+			</div>
+			<div class="col-md-12">
+				<div class="trade-menu">
+					<ul class="trade-list">
+						<li>
+							<div class="image-boxx text-center">
+								<span class="active">
+									<img src="images/forex.webp" class="img-fluid">
+								</span>
+								<h4>Forex</h4>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<span>
+									<img src="images/indices.webp" class="img-fluid">
+								</span>
+								<h4>Indices</h4>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<span>
+									<img src="images/comm.webp" class="img-fluid">
+								</span>
+								<h4>Commodities</h4>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<span>
+									<img src="images/stocks.webp" class="img-fluid">
+								</span>
+								<h4>Stocks</h4>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<span>
+									<img src="images/cryptos.webp" class="img-fluid">
+								</span>
+								<h4>Cryptos</h4>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
