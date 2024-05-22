af502d643490a3d5d50ba71a5bcd0616c80dad47
amitfxcareers <amit@fxcareers.com>
2024-05-22 11:35:53 +0530

    Why Trade Forex with NXG Markets section added to forex page

--- forex.php
+++ forex.php
@@ -1,6 +1,6 @@
 <?php include_once('./include/header.php') ?>
 
-
+<!-- Trade Forex -->
 <section class="trade-section">
 	<div class="container">
 		<div class="row">
@@ -69,5 +69,58 @@
 		</div>
 	</div>
 </section>
-<?php include_once('./include/why-trade-with-nxg-markets.php') ?>
+<!-- Trade Forex -->
+
+<!-- Why Trade Forex with NXG Markets? -->
+<section class="why-trade-with-nxg">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <div class="mb-5">
+               <h4 class="text-white">
+                  Trading Instruments
+               </h4>
+               <h2 class="text-white">
+                  Why Trade Forex with NXG Markets?
+               </h2>
+            </div>
+            <div class="icon-box">
+               <div class="icon">
+               	<!-- <i class="fas fa-fingerprint"></i> -->
+               	<img src="images/competitive-trading-conditions.png" class="img-fluid">
+               </div>
+               <h4 class="title">Competitive Trading Conditions</h4>
+               <p class="description">Benefit from tight spreads, low commissions, and lightning-fast order execution, At Sub Second level. We prioritize competitive pricing to optimize your trading strategies.</p>
+            </div>
+            <div class="icon-box">
+               <div class="icon"><i class="fas fa-fingerprint"></i></div>
+               <h4 class="title">Wide Range of Currency Pairs</h4>
+               <p class="description">Explore 40+ currency pairs, including major global currencies with high liquidity. Diversify your portfolio and seize opportunities in various market movements.</p>
+            </div>
+            <div class="icon-box">
+               <div class="icon"><i class="fas fa-fingerprint"></i></div>
+               <h4 class="title">Advanced Trading Platforms</h4>
+               <p class="description">Choose from MT5, cTrader. Trade seamlessly on the platform that suits your style and preferences.</p>
+            </div>
+            <div class="icon-box">
+               <div class="icon"><i class="fas fa-fingerprint"></i></div>
+               <h4 class="title">Secure and Convenient Transactions</h4>
+               <p class="description">Enjoy multiple deposit and withdrawal options, ensuring secure and hassle-free fund management.</p>
+            </div>
+         </div>
+         <div class="col-md-6 text-center">
+            <img src="images/why-trade-forex.png" class="img-fluid" width="80%">
+         </div>
+      </div>
+      <div class="row mt-4">
+      	<div class="col-md-12">
+      		<div class="text-center">
+				<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-phone" aria-hidden="true"></i>Get In Touch</a>
+			</div>
+      	</div>
+      </div>
+   </div>
+</section>
+<!-- Why Trade Forex with NXG Markets? -->
+
 <?php include_once('./include/footer.php') ?>
