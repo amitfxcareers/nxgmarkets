4280011a21a0759b3925eeddb7fca7e61d7c5597
amitfxcareers <amit@fxcareers.com>
2024-05-22 11:29:41 +0530

    include files removed and code added to files

--- forex.php
+++ forex.php
@@ -1,5 +1,73 @@
 <?php include_once('./include/header.php') ?>
 
-<?php include_once('./include/trade-forex.php') ?>
+
+<section class="trade-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="main-heading">Trade Forex</h2>
+				<p class="text-center">
+					Forex is the worldwide marketplace for currency exchange, providing traders with opportunities to profit from fluctuations in exchange rates. With a daily trade volume of $6.5 trillion, Forex offers substantial potential for both novice and professional traders. Open your trading account with NXG Markets and trade the forex markets securely.
+				</p>
+			</div>
+			<div class="col-md-12">
+				<div class="trade-menu">
+					<ul class="trade-list">
+						<li>
+							<div class="image-boxx text-center">
+								<a href="forex.php">
+									<span class="active">
+										<img src="images/forex.webp" class="img-fluid">
+									</span>
+									<h4>Forex</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="indices.php">
+									<span>
+										<img src="images/indices.webp" class="img-fluid">
+									</span>
+									<h4>Indices</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="commodities.php">
+									<span>
+										<img src="images/comm.webp" class="img-fluid">
+									</span>
+									<h4>Commodities</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="stocks.php">
+									<span>
+										<img src="images/stocks.webp" class="img-fluid">
+									</span>
+									<h4>Stocks</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="cryptocurrencies.php">
+									<span>
+										<img src="images/cryptos.webp" class="img-fluid">
+									</span>
+									<h4>Cryptos</h4>
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <?php include_once('./include/why-trade-with-nxg-markets.php') ?>
 <?php include_once('./include/footer.php') ?>
--- indices.php
+++ indices.php
@@ -1,5 +1,72 @@
 <?php include_once('./include/header.php') ?>
 
 
-<?php include_once('./include/trade-indices.php') ?>
+<section class="trade-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="main-heading">Trade Indices</h2>
+				<p class="text-center">
+					Indices are bundles of stocks from particular markets or sectors, offering a snapshot of market performance. Trading these indices is simple and advantageous, providing a way to speculate on the overall market or specific sectors. With user-friendly features, NXG Markets ensures a seamless experience in exploring and trading indices.
+				</p>
+			</div>
+			<div class="col-md-12">
+				<div class="trade-menu">
+					<ul class="trade-list">
+						<li>
+							<div class="image-boxx text-center">
+								<a href="forex.php">
+									<span>
+										<img src="images/forex.webp" class="img-fluid">
+									</span>
+									<h4>Forex</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="indices.php">
+									<span class="active">
+										<img src="images/indices.webp" class="img-fluid">
+									</span>
+									<h4>Indices</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="commodities.php">
+									<span>
+										<img src="images/comm.webp" class="img-fluid">
+									</span>
+									<h4>Commodities</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="stocks.php">
+									<span>
+										<img src="images/stocks.webp" class="img-fluid">
+									</span>
+									<h4>Stocks</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="cryptocurrencies.php">
+									<span>
+										<img src="images/cryptos.webp" class="img-fluid">
+									</span>
+									<h4>Cryptos</h4>
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <?php include_once('./include/footer.php') ?>
