2f5d17b08a6c274faecba9ffe9155a7b522830f6
amitfxcareers <amit@fxcareers.com>
2024-05-22 15:40:23 +0530

    Common sections added to MT5, Ctrader and CopyTrading

--- copytrading.php
+++ copytrading.php
@@ -1,3 +1,55 @@
 <?php include_once('./include/header.php') ?>
 
+
+<?php include_once('./include/why-nxg-markets.php') ?>
+
+<!-- Looking for More? -->
+<section class="trade-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="main-heading">Looking for More?</h2>
+			</div>
+			<div class="col-md-12 mt-4">
+				<div class="trade-menu">
+					<ul class="trade-list-ul">
+						<li>
+							<div class="image-boxx text-center">
+								<a href="mt5.php">
+									<span>
+										<img src="images/MT5.webp" class="img-fluid">
+									</span>
+									<h4>Meta Trader 5</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="ctrader.php">
+									<span>
+										<img src="images/cTrader.webp" class="img-fluid">
+									</span>
+									<h4>cTrader</h4>
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="image-boxx text-center">
+								<a href="copytrading.php">
+									<span class="active">
+										<img src="images/copy-trading.webp" class="img-fluid">
+									</span>
+									<h4>CopyTrading</h4>
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Looking for More? -->
+
+<?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
