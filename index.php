6ac3598e16fe84c0df28ad5142cd23445ee7c8f1
amitfxcareers <amit@fxcareers.com>
2024-05-23 14:57:47 +0530

    banner added and images added

--- index.php
+++ index.php
@@ -17,70 +17,18 @@
       				<h2>
       					Refer & Shop Affiliate Contest
       				</h2>
-      				<ul>
+      				<ul class="banner-ul">
       					<li>
-      					    <img src="images/refer-client-to-nxg-markets.png" class="img-fluid">	Refer Clients to NXG Markets
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      					    Refer Clients to NXG Markets
       					</li>
       					<li>
-      						<img src="images/meet-deposite-and-volume-targets.png">
+      						<img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
       						Meet Deposit & Volume Targets
       					</li>
       					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
       						Win Amazon Shopping Vouchers upto $1000
-      					</li>
-      				</ul>
-      			</div>
-      		</div>
-      		<div class="col-md-6">
-      			<img src="images/slide3.webp" class="img-fluid">
-      		</div>
-      	</div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <div class="container">
-      	<div class="row align-items-center">
-      		<div class="col-md-6">
-      			<div class="banner-content-pointers">
-      				<h2>
-      					Fixed Rate Deposit & Withdrawal 
-      				</h2>
-      				<ul>
-      					<li>
-      						Exceptional Rate
-      					</li>
-      					<li>
-      						Security Assured
-      					</li>
-      					<li>
-      						Stability and Growth
-      					</li>
-      				</ul>
-      			</div>
-      		</div>
-      		<div class="col-md-6">
-      			<img src="images/slide4.webp" class="img-fluid">
-      		</div>
-      	</div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <div class="container">
-      	<div class="row align-items-center">
-      		<div class="col-md-6">
-      			<div class="banner-content-pointers">
-      				<h2>
-      					Trade Better, Trade Smarter<br>Trade with NXG Markets
-      				</h2>
-      				<ul>
-      					<li>
-      						Refer Clients to NXG Markets
-      					</li>
-      					<li>
-      						Refer Clients to NXG Markets
-      					</li>
-      					<li>
-      						Refer Clients to NXG Markets
       					</li>
       				</ul>
       			</div>
@@ -97,23 +45,86 @@
       		<div class="col-md-6">
       			<div class="banner-content-pointers">
       				<h2>
+      					Fixed Rate Deposit & Withdrawal 
+      				</h2>
+      				<ul class="banner-ul">
+      					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      						Exceptional Rate
+      					</li>
+      					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      						Security Assured
+      					</li>
+      					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      						Stability and Growth
+      					</li>
+      				</ul>
+      			</div>
+      		</div>
+      		<div class="col-md-6">
+      			<img src="https://www.nxgmarkets.com/wp-content/uploads/2024/01/NXG-Banner-image-1.png" class="img-fluid">
+      		</div>
+      	</div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+      	<div class="row align-items-center">
+      		<div class="col-md-6">
+      			<div class="banner-content-pointers">
+      				<h2>
+      					Trade Better, Trade Smarter Trade with NXG Markets
+      				</h2>
+      				<ul class="banner-ul">
+      					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      						Refer Clients to NXG Markets
+      					</li>
+      					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      						Refer Clients to NXG Markets
+      					</li>
+      					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
+      						Refer Clients to NXG Markets
+      					</li>
+      				</ul>
+      			</div>
+      		</div>
+      		<div class="col-md-6">
+      			<img src="images/slide3.png" class="img-fluid">
+      		</div>
+      	</div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+      	<div class="row align-items-center">
+      		<div class="col-md-6">
+      			<div class="banner-content-pointers">
+      				<h2>
       					Get 100% Deposit Bonus
       				</h2>
-      				<ul>
+      				<ul class="banner-ul">
       					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
       						Upto $100
       					</li>
       					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
       						New ‘START ACCOUNT’
       					</li>
       					<li>
+                  <img src="images/meet-deposite-and-volume-targets.png" class="img-fluid">
       						Offer Valid Till 31st May
       					</li>
       				</ul>
       			</div>
       		</div>
       		<div class="col-md-6">
-      			<img src="images/slide2.webp" class="img-fluid">
+      			<img src="images/slider4.png" class="img-fluid">
       		</div>
       	</div>
       </div>
@@ -128,7 +139,13 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-
 <!-- Banner -->
 
+<section class="banner-factoids">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12"></div>
+    </div>
+  </div>
+</section>
 <?php include_once('./include/footer.php') ?>
