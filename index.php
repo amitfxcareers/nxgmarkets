d11c2a1882c56c8e9308ef56cd73cb262710c72a
amitfxcareers <amit@fxcareers.com>
2024-05-23 15:58:26 +0530

    Home page factoids and tabs created

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include_once('./include/header.php') ?>
 
 <!-- Banner -->
-<div id="carouselExampleCaptions" class="carousel slide">
+<div id="carouselExampleCaptions" class="carousel slide home-banner">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -141,11 +141,126 @@
 </div>
 <!-- Banner -->
 
+<!-- Banner Factoids -->
 <section class="banner-factoids">
   <div class="container">
     <div class="row">
-      <div class="col-md-12"></div>
+      <div class="col-md-12">
+        <div class="banner_factoids">
+          <ul>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  1:1000
+                </h2>
+                <p class="text-white">Flexible Leverage<br>
+                  <strong>Enjoy High Leverage</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  100+
+                </h2>
+                <p class="text-white">Trading Instruments<br>
+                  <strong>Diverse Asset Selection</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  0.0
+                </h2>
+                <p class="text-white">Spreads from<br>
+                  <strong>Lowest Spreads Possible</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  Fast
+                </h2>
+                <p class="text-white">Withdrawals<br>
+                  <strong>Fast and Secure Transactions</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  Regulated
+                </h2>
+                <p class="text-white">Broker<br>
+                  <strong>Trusted & Regulated Trading</strong>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </section>
+<!-- Banner Factoids -->
+
+<!-- Top Traded Markets -->
+<section class="top-traded-markets">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="d-flex align-items-start">
+          <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <button class="nav-link active" id="v-pills-forex-tab" data-bs-toggle="pill" data-bs-target="#v-pills-forex" type="button" role="tab" aria-controls="v-pills-forex" aria-selected="true">Forex</button>
+            <button class="nav-link" id="v-pills-commodities-tab" data-bs-toggle="pill" data-bs-target="#v-pills-commodities" type="button" role="tab" aria-controls="v-pills-commodities" aria-selected="false">Commodities</button>
+            <button class="nav-link" id="v-pills-indices-tab" data-bs-toggle="pill" data-bs-target="#v-pills-indices" type="button" role="tab" aria-controls="v-pills-indices" aria-selected="false">Messages</button>
+            <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+            <button class="nav-link" id="v-pills-stocks-tab" data-bs-toggle="pill" data-bs-target="#v-pills-stocks" type="button" role="tab" aria-controls="v-pills-stocks" aria-selected="false">Stocks</button>
+          </div>
+          <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" id="v-pills-forex" role="tabpanel" aria-labelledby="v-pills-forex-tab">
+              <p>EUR/USD</p>
+              <p>USD/IPY</p>
+              <p>GBP/USD</p>
+              <p>AUD/USD</p>
+            </div>
+            <div class="tab-pane fade" id="v-pills-commodities" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+              <p>XAU/USD (Gold)</p>
+              <p>XAG/USD (Silver)</p>
+              <p>BRENT (Crude Oil)</p>
+              <p>NGAS (Natural Gas)</p>
+            </div>
+            <div class="tab-pane fade" id="v-pills-indices" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+              <p>S&amp;P 500 (US)</p>
+              <p>Dow Jones Industrial Average (US)</p>
+              <p>FTSE 100 (UK)</p>
+              <p>DAX 30 (Germany)</p>
+            </div>
+            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+              <p>Bitcoin (BTC)</p>
+              <p>Ethereum (ETH)</p>
+              <p>Ripple (XRP)</p>
+              <p>Litecoin (LTC)</p>
+            </div>
+            <div class="tab-pane fade" id="v-pills-stocks" role="tabpanel" aria-labelledby="v-pills-stocks-tab">
+              <p>Bitcoin (BTC)</p>
+              <p>Ethereum (ETH)</p>
+              <p>Ripple (XRP)</p>
+              <p>Litecoin (LTC)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <h4>
+          A Trusted Financial Markets Trading Platform
+        </h4>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Top Traded Markets -->
+
 <?php include_once('./include/footer.php') ?>
