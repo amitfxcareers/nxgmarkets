3e7f926b4366e128287a9df6b412cbf5131987b9
amitfxcareers <amit@fxcareers.com>
2024-05-24 17:40:32 +0530

    Banner added to inner pages

--- about.php
+++ about.php
@@ -1,4 +1,25 @@
 <?php include_once('./include/header.php') ?>
+<!-- Banner -->
+<section class="inner-banner" style="background-image: url(images/about-banner.png);">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="banner_content ">
+					<h2 class="text-white">
+						About Us
+					</h2>
+					<p class="text-white">
+						Welcome to NXG Markets – Your Trading Companion for Precision Trading and Proven Results.
+					</p>
+					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Banner -->
+
+
 
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
--- contact-us.php
+++ contact-us.php
@@ -1,4 +1,22 @@
 <?php include_once('./include/header.php') ?>
-
+<!-- Banner -->
+<section class="inner-banner" style="background-image: url(images/advantages-banner.png);">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="banner_content ">
+					<h2 class="text-white">
+						Advantages
+					</h2>
+					<p class="text-white">
+						Explore our advantages and experience the best opportunities for collaboration, backed by concrete figures.
+					</p>
+					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Banner -->
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
