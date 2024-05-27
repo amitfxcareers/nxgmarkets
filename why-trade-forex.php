97cc0b7f87a78efb21fd28008a0bb355a4b54dd9
amitfxcareers <amit@fxcareers.com>
2024-05-27 13:25:45 +0530

    Why Trade Forex page done

--- why-trade-forex.php
+++ why-trade-forex.php
@@ -19,6 +19,181 @@
 </section>
 <!-- Banner -->
 
+<!-- Banner Factoids -->
+<section class="banner-why-factoids">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="banner_factoids">
+          <ul>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  1:1000
+                </h2>
+                <p class="text-white">Flexible Leverage<br>
+                  <strong>Enjoy High Leverage</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  100+
+                </h2>
+                <p class="text-white">Trading Instruments<br>
+                  <strong>Diverse Asset Selection</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  0.0
+                </h2>
+                <p class="text-white">Spreads from<br>
+                  <strong>Lowest Spreads Possible</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  Fast
+                </h2>
+                <p class="text-white">Withdrawals<br>
+                  <strong>Fast and Secure Transactions</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  Regulated
+                </h2>
+                <p class="text-white">Broker<br>
+                  <strong>Trusted & Regulated Trading</strong>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Banner Factoids -->
+
+<!-- Why Trade Forex -->
+
+<section class="why-trade-forex-section">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <h5 class="text-black">
+               Forex – The Most Liquid Market
+            </h5>
+            <h2 class="text-primary">
+               Why Trade Forex?
+            </h2>
+            <p>
+               Forex is the most traded market in the world and when you understand the benefits of the market, it is easy to understand why.
+            </p>
+            <p>
+               Explore the Most Liquid Market: Why Choose Forex
+            </p>
+            <p>
+               Forex stands out as the world’s most traded market, offering numerous advantages for savvy traders.
+            </p>
+         </div>
+         <div class="col-md-6">
+            <lottie-player src="images/lottie-json/trade-forex.json" style="width: 100%; height: 400px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
+      </div>
+      <div class="row gy-4">
+         <div class="col-md-4">
+            <div class="why-image-box">
+               <div class="main-head">
+                  <span>1</span>
+                  <h5 class="text-white">
+                     24/5 Trading
+                  </h5>
+               </div>
+               <p class="text-white">
+                  Forex trading distinguishes itself by operating 24 hours a day, five days a week, transcending time zones and enabling you to trade at your convenience, without being constrained by daylight hours.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="why-image-box">
+               <div class="main-head">
+                  <span>2</span>
+                  <h5 class="text-white">
+                     Transparent Costs
+                  </h5>
+               </div>
+               <p class="text-white">
+                  Trading at NXGMarkets.com is straightforward and transparent; all costs are incorporated into the spread, ensuring no hidden fees or commissions. This transparency allows you to confidently understand the cost of your trades.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="why-image-box">
+               <div class="main-head">
+                  <span>3</span>
+                  <h5 class="text-white">
+                     Versatile Market Opportunities
+                  </h5>
+               </div>
+               <p class="text-white">
+                  The forex market provides traders with a distinct advantage, allowing opportunities in both rising and falling markets. Unlike other markets, there are no restrictions or additional costs for short selling.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="why-image-box">
+               <div class="main-head">
+                  <span>4</span>
+                  <h5 class="text-white">
+                     Trade more with less
+                  </h5>
+               </div>
+               <p class="text-white">
+                  Trade more with less in Forex, thanks to leverage, enabling you to enter the market with a fraction of the typical capital required. While leverage can boost gains, it's crucial to understand the risks associated with margin trading, as it may also amplify losses.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="why-image-box">
+               <div class="main-head">
+                  <span>5</span>
+                  <h5 class="text-white">
+                     Unparalleled Liquidity
+                  </h5>
+               </div>
+               <p class="text-white">
+                  Forex, with a daily turnover of $6.6 trillion,* stands as the world's most liquid market. This liquidity facilitates prompt responses to currency fluctuations, 24 hours a day, 5 days a week, leading to more actionable prices compared to other financial markets.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="why-image-box">
+               <div class="main-head">
+                  <span>6</span>
+                  <h5 class="text-white">
+                     Diverse Market Access
+                  </h5>
+               </div>
+               <p class="text-white">
+                  Forex trading provides exposure to a wide range of global markets. While major currencies dominate, opportunities extend to emerging markets like the Mexican Peso (MXN) and Polish Zloty (PLN).
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<!-- Why Trade Forex -->
 
 <!-- Why Choose NXG Markets for Trading? -->
 <section class="why-trade-with-nxg">
