491e965a994aa0528497d1700479b8647e66ab83
amitfxcareers <amit@fxcareers.com>
2024-05-27 15:01:34 +0530

    Webinars Page done

--- webinars.php
+++ webinars.php
@@ -19,6 +19,36 @@
 </section>
 <!-- Banner -->
 
+<!-- Webinars-->
+<section class="trading-webinars">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<h5 class="text-black">
+					Step ahead in Forex: Interactive Webinars
+				</h5>
+				<h2 class="text-primary">
+					Webinars
+				</h2>
+				<p>
+					Participate in interactive webinars at NXG Markets. Hosted by industry professionals, our webinars delve into market analysis, trading strategies, risk management, and more. Attend live sessions to learn from experts, ask questions, and gain valuable insights into the financial markets.
+				</p>
+				<p>
+					Register now for upcoming webinars and broaden your trading knowledge.
+				</p>
+				<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+			</div>
+			<div class="col-md-6">
+				<lottie-player src="images/lottie-json/webinars.json" style="width: 100%; height: 400px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Webinars -->
+
+<!-- Free Demo Account -->
+<?php include_once('./include/free-demo-account.php') ?>
+<!-- Free Demo Account -->
 
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
