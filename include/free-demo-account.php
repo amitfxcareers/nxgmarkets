7e8e7e23174977c5bdd0e9ad4ba6c3463d4b28b6
amitfxcareers <amit@fxcareers.com>
2024-05-27 14:50:56 +0530

    Free Demo Account commno section crreated

---
+++ include/free-demo-account.php
@@ -0,0 +1,23 @@
+<section class="free-demo-account">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<img src="images/new-to-trading.png" class="img-fluid">
+			</div>
+			<div class="col-md-6">
+				<h5 class="text-white">
+					Free Demo Account
+				</h5>
+				<h2 class="text-white">
+					Just Starting Out in Trading?
+				</h2>
+				<p class="text-white">
+					Refine your forex trading skills with a demo account from NXG Markets. Open a risk-free demo account today to explore our trading platforms, test strategies, and boost your confidence. It’s an ideal chance to learn and enhance your skills without risking actual funds.
+				</p>
+				<div class="get-in-touch mt-5">
+					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Get Free Demo</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
