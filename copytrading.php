4e8ce5f431bfb676121b589b8231316e8ad2a07b
amitfxcareers <amit@fxcareers.com>
2024-05-28 16:03:11 +0530

    Copytrading tab created

--- copytrading.php
+++ copytrading.php
@@ -1,73 +1,243 @@
 <?php include_once('./include/header.php') ?>
 <!-- Banner -->
 <section class="inner-banner" style="background-image: url(images/copytrading_banner.webp);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="banner_content ">
-					<h2 class="text-white">
-						CopyTrading
-					</h2>
-					<p class="text-white">
-						Increase your profits by trading over 100 assets through Copy Trading – A modern way to leverage expert strategies.
-					</p>
-					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  CopyTrading
+               </h2>
+               <p class="text-white">
+                  Increase your profits by trading over 100 assets through Copy Trading – A modern way to leverage expert strategies.
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Banner -->
-
+<!-- Trading platform Copytrading -->
+<section class="trading-platform">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <p>
+               Tap into the knowledge of expert traders using CopyTrading at NXG Markets. Follow and effortlessly copy the trades of experienced traders, benefiting from their expertise. Choose from a range of top-performing traders to diversify your trading portfolio effortlessly. Register now and get into the world of CopyTrading.
+            </p>
+            <h5 class="text-black fw-600">
+               NXG Markets Copy Trading
+            </h5>
+            <h2 class="text-primary fw-800">
+               Explore Copy Trading at NXG Markets
+            </h2>
+            <p>
+               Multiply profits by following successful traders, diversify effortlessly, and fund your account conveniently with NXG Markets. Explore a seamless trading experience with expert strategies, diverse portfolios, and flexible funding options—all in one place.
+            </p>
+         </div>
+         <div class="col-md-6">
+            <lottie-player src="images/lottie-json/copytrading.json" style="width: 100%; height: 300px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Trading platform Copytrading -->
+<section class="how-copy-trading-works">
+   <div class="bg_image">
+      <div class="container">
+         <div class="row gy-4">
+            <div class="col-md-12">
+               <h2 class="text-white fw-800">
+                  How Copy Trading Works
+               </h2>
+            </div>
+            <div class="col-md-8">
+               <div class="container">
+                  <div class="row gy-4 copy-trading-work">
+                     <div class="col-md-6">
+                        <h4 class="fw-800">1. Two Account Types:</h4>
+                        <p>Copy trading has two key players – the master account, guided by an experienced trader, and the follower account, where an investor shadows the master's moves.</p>
+                     </div>
+                     <div class="col-md-6">
+                        <h4 class="fw-800">2. Platform:</h4>
+                        <p>Both masters and followers unite on platforms like Etoro and Darwinex, ensuring an easy connection for copy trading interactions.</p>
+                     </div>
+                     <div class="col-md-6">
+                        <h4 class="fw-800">3. Trades:</h4>
+                        <p>When the master executes a trade, it duplicates the follower's account. Adjustments, like modifying stop loss, mimic seamlessly between the two accounts.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Why NXG Markets? -->
+<!-- Which Trader Will you be -->
+<section class="which-trader">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="which-trader-will">
+               <ul class="nav nav-tabs" id="myTab" role="tablist">
+                  <li class="nav-item" role="presentation">
+                     <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Follower</button>
+                  </li>
+                  <li class="nav-item" role="presentation">
+                     <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Master Trader</button>
+                  </li>
+               </ul>
+               <div class="tab-content" id="myTabContent">
+                  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                     <div class="container">
+                        <div class="row mt-4">
+                           <div class="col-md-12 text-center">
+                              <h5 class="text-black fw-600">
+                                 Copy Trading
+                              </h5>
+                              <h2 class="text-primary fw-800">
+                                 Advantages of Copy Trading
+                              </h2>
+                           </div>
+                        </div>
+                        <div class="row mt-5">
+                           <div class="col-md-12 text-center">
+                              <img src="images/copy-trade.png" class="img-fluid" width="60%">
+                           </div>
+                        </div>
+                        <div class="row mt-5 gy-4 justify-content-center text-center">
+                           <div class="col-lg-4">
+                              <div class="main-feature">
+                                 <figure>
+                                    <img src="images/Access-to-Expert.png" class="img-fluid">
+                                 </figure>
+                                 <div class="feature-content">
+                                    <h4 class="text-primary fw-600">
+                                       Access to Skilled Traders
+                                    </h4>
+                                    <p>
+                                       Access a variety of accomplished traders on the CopyTrading platform. Replicate their trades effortlessly to leverage their expertise for your benefit.
+                                    </p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-lg-4 col-md-6">
+                              <div class="main-feature">
+                                 <figure>
+                                    <img src="images/Diversification-of-Trading-Portfolio.png" class="img-fluid">
+                                 </figure>
+                                 <div class="feature-content">
+                                    <h4 class="text-primary fw-600">
+                                       Diversify Your Portfolio
+                                    </h4>
+                                    <p>
+                                       Explore the flexibility of CopyTrading to diversify your portfolio. Follow multiple successful traders concurrently, spreading risks and potentially boosting your overall trading performance.
+                                    </p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-md-6 col-lg-4">
+                              <div class="main-feature">
+                                 <figure>
+                                    <img src="images/advanced-charting-and-analysis.png" class="img-fluid">
+                                 </figure>
+                                 <div class="feature-content">
+                                    <h4 class="text-primary fw-600">
+                                       No Trading Background Needed
+                                    </h4>
+                                    <p>
+                                       Even if you are a beginner, CopyTrading makes market participation easy. Automatically mimic trades of seasoned traders, eliminating the need for extensive market knowledge.
+                                    </p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-md-6 col-lg-4">
+                              <div class="main-feature">
+                                 <figure>
+                                    <img src="images/fast-order-execution.png" class="img-fluid">
+                                 </figure>
+                                 <h4 class="text-primary fw-600">
+                                    Control and Adaptability
+                                 </h4>
+                                 <p>
+                                    With CopyTrading, you dictate the traders to follow and how much capital to allocate. Customize settings according to your risk tolerance and preferences for ultimate control.
+                                 </p>
+                              </div>
+                           </div>
+                           <div class="col-md-6 col-lg-4">
+                              <div class="main-feature">
+                                 <figure>
+                                    <img src="images/multiple-order-types.png" class="img-fluid">
+                                 </figure>
+                                 <h4 class="text-primary fw-600">
+                                    Learning Opportunities
+                                 </h4>
+                                 <p>
+                                    Observe and analyze successful traders’ strategies for a valuable learning experience. Enhance your trading skills and knowledge over time with CopyTrading.
+                                 </p>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Which Trader Will you be -->
 <?php include_once('./include/why-nxg-markets.php') ?>
-
+<!-- Why NXG Markets? -->
 <!-- Looking for More? -->
 <section class="trade-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2 class="main-heading">Looking for More?</h2>
-			</div>
-			<div class="col-md-12 mt-4">
-				<div class="trade-menu">
-					<ul class="trade-list-ul">
-						<li>
-							<div class="image-boxx text-center">
-								<a href="mt5.php">
-									<span>
-										<img src="images/MT5.webp" class="img-fluid">
-									</span>
-									<h4>Meta Trader 5</h4>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="image-boxx text-center">
-								<a href="ctrader.php">
-									<span>
-										<img src="images/cTrader.webp" class="img-fluid">
-									</span>
-									<h4>cTrader</h4>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="image-boxx text-center">
-								<a href="copytrading.php">
-									<span class="active">
-										<img src="images/copy-trading.webp" class="img-fluid">
-									</span>
-									<h4>CopyTrading</h4>
-								</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="main-heading">Looking for More?</h2>
+         </div>
+         <div class="col-md-12 mt-4">
+            <div class="trade-menu">
+               <ul class="trade-list-ul">
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="mt5.php">
+                           <span>
+                           <img src="images/MT5.webp" class="img-fluid">
+                           </span>
+                           <h4>Meta Trader 5</h4>
+                        </a>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="ctrader.php">
+                           <span>
+                           <img src="images/cTrader.webp" class="img-fluid">
+                           </span>
+                           <h4>cTrader</h4>
+                        </a>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="copytrading.php">
+                           <span class="active">
+                           <img src="images/copy-trading.webp" class="img-fluid">
+                           </span>
+                           <h4>CopyTrading</h4>
+                        </a>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Looking for More? -->
-
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
--- ctrader.php
+++ ctrader.php
@@ -1,35 +1,34 @@
 <?php include_once('./include/header.php') ?>
 <!-- Banner -->
 <section class="inner-banner" style="background-image: url(images/ctrader-banner.webp);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="banner_content ">
-					<h2 class="text-white">
-						cTrader
-					</h2>
-					<p class="text-white">
-						Effortlessly trade over 300 asset classes on cTrader – A feature-rich trading platform designed for the modern trader.
-					</p>
-					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  cTrader
+               </h2>
+               <p class="text-white">
+                  Effortlessly trade over 300 asset classes on cTrader – A feature-rich trading platform designed for the modern trader.
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Banner -->
-
 <!-- Trading platform CTrader -->
 <section class="trading-platform">
-	<div class="container">
-		<div class="row gy-4">
-	        <div class="col-md-12">
-	            <p class="text-center platform-intro">
-	               Take your trading to the next level with MT5 at NXG Markets. Benefit from enhanced features, including additional charting tools and economic calendar integration. Access MT5 on desktop, web, and mobile devices to leverage its advanced capabilities. Register now and elevate your trading with MT5.
-	            </p>
-	        </div>
-      	</div>
-      	<div class="row mt-4">
+   <div class="container">
+      <div class="row gy-4">
+         <div class="col-md-12">
+            <p class="text-center platform-intro">
+               Take your trading to the next level with MT5 at NXG Markets. Benefit from enhanced features, including additional charting tools and economic calendar integration. Access MT5 on desktop, web, and mobile devices to leverage its advanced capabilities. Register now and elevate your trading with MT5.
+            </p>
+         </div>
+      </div>
+      <div class="row mt-4">
          <div class="col-md-12 text-center">
             <h5 class="text-black fw-600">
                Trading Platform
@@ -63,37 +62,37 @@
          <div class="col-lg-4 col-md-6">
             <div class="main-feature">
                <figure>
-               		<img src="images/algorithmic-trading.png" class="img-fluid">
+                  <img src="images/algorithmic-trading.png" class="img-fluid">
                </figure>
                <div class="feature-content">
-	               	<h4 class="text-primary fw-600">
-	                  Algorithmic Trading
-	               </h4>
-	               <p>
-	                  Create or use algorithmic trading strategies with cTrader's user-friendly platform, allowing automated trading based on preset rules and conditions.
-	               </p>
+                  <h4 class="text-primary fw-600">
+                     Algorithmic Trading
+                  </h4>
+                  <p>
+                     Create or use algorithmic trading strategies with cTrader's user-friendly platform, allowing automated trading based on preset rules and conditions.
+                  </p>
                </div>
             </div>
          </div>
          <div class="col-md-6 col-lg-4">
             <div class="main-feature">
                <figure>
-               		<img src="images/advanced-charting-and-analysis.png" class="img-fluid">
+                  <img src="images/advanced-charting-and-analysis.png" class="img-fluid">
                </figure>
                <div class="feature-content">
-	               	<h4 class="text-primary fw-600">
-	                  Advanced Charting and Analysis
-	               </h4>
-	               <p>
-	                  Leverage advanced charting, a variety of technical indicators, and drawing tools on cTrader for detailed market analysis, aiding in making well-informed trading decisions.
-	               </p>
+                  <h4 class="text-primary fw-600">
+                     Advanced Charting and Analysis
+                  </h4>
+                  <p>
+                     Leverage advanced charting, a variety of technical indicators, and drawing tools on cTrader for detailed market analysis, aiding in making well-informed trading decisions.
+                  </p>
                </div>
             </div>
          </div>
          <div class="col-md-6 col-lg-4">
             <div class="main-feature">
                <figure>
-               		<img src="images/fast-order-execution.png" class="img-fluid">
+                  <img src="images/fast-order-execution.png" class="img-fluid">
                </figure>
                <h4 class="text-primary fw-600">
                   Fast Order Execution
@@ -105,9 +104,9 @@
          </div>
          <div class="col-md-6 col-lg-4">
             <div class="main-feature">
-            	<figure>
-            		<img src="images/multiple-order-types.png" class="img-fluid">
-            	</figure>
+               <figure>
+                  <img src="images/multiple-order-types.png" class="img-fluid">
+               </figure>
                <h4 class="text-primary fw-600">
                   Various Order Types
                </h4>
@@ -118,68 +117,64 @@
          </div>
       </div>
       <div class="row">
-      	<div class="col-md-12">
-      		<div class="text-center mt-5">
-      			<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-      		</div>
-      	</div>
+         <div class="col-md-12">
+            <div class="text-center mt-5">
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
       </div>
-	</div>
+   </div>
 </section>
-
 <!-- Trading platform CTrader -->
-
 <!-- Why NXG Markets? -->
 <?php include_once('./include/why-nxg-markets.php') ?>
 <!-- Why NXG Markets? -->
-
 <!-- Looking for More? -->
 <section class="trade-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2 class="main-heading">Looking for More?</h2>
-			</div>
-			<div class="col-md-12 mt-4">
-				<div class="trade-menu">
-					<ul class="trade-list-ul">
-						<li>
-							<div class="image-boxx text-center">
-								<a href="mt5.php">
-									<span>
-										<img src="images/MT5.webp" class="img-fluid">
-									</span>
-									<h4>Meta Trader 5</h4>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="image-boxx text-center">
-								<a href="ctrader.php">
-									<span class="active">
-										<img src="images/cTrader.webp" class="img-fluid">
-									</span>
-									<h4>cTrader</h4>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="image-boxx text-center">
-								<a href="copytrading.php">
-									<span>
-										<img src="images/copy-trading.webp" class="img-fluid">
-									</span>
-									<h4>CopyTrading</h4>
-								</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="main-heading">Looking for More?</h2>
+         </div>
+         <div class="col-md-12 mt-4">
+            <div class="trade-menu">
+               <ul class="trade-list-ul">
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="mt5.php">
+                           <span>
+                           <img src="images/MT5.webp" class="img-fluid">
+                           </span>
+                           <h4>Meta Trader 5</h4>
+                        </a>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="ctrader.php">
+                           <span class="active">
+                           <img src="images/cTrader.webp" class="img-fluid">
+                           </span>
+                           <h4>cTrader</h4>
+                        </a>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="copytrading.php">
+                           <span>
+                           <img src="images/copy-trading.webp" class="img-fluid">
+                           </span>
+                           <h4>CopyTrading</h4>
+                        </a>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Looking for More? -->
-
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
