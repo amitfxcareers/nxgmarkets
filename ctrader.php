0a4adfc633bc0c9bd89abbe2193e47a82addd6e5
amitfxcareers <amit@fxcareers.com>
2024-05-28 12:45:00 +0530

    cTrader page done

--- ctrader.php
+++ ctrader.php
@@ -19,7 +19,119 @@
 </section>
 <!-- Banner -->
 
+<!-- Trading platform CTrader -->
+<section class="trading-platform">
+	<div class="container">
+		<div class="row gy-4">
+	        <div class="col-md-12">
+	            <p class="text-center platform-intro">
+	               Take your trading to the next level with MT5 at NXG Markets. Benefit from enhanced features, including additional charting tools and economic calendar integration. Access MT5 on desktop, web, and mobile devices to leverage its advanced capabilities. Register now and elevate your trading with MT5.
+	            </p>
+	        </div>
+      	</div>
+      	<div class="row mt-4">
+         <div class="col-md-12 text-center">
+            <h5 class="text-black fw-600">
+               Trading Platform
+            </h5>
+            <h2 class="text-primary fw-800">
+               Advantages of Using cTrader
+            </h2>
+         </div>
+      </div>
+      <div class="row mt-5">
+         <div class="col-md-12 text-center">
+            <img src="images/ctrader-inner.png" class="img-fluid" width="60%">
+         </div>
+      </div>
+      <div class="row mt-5 gy-4 justify-content-center text-center">
+         <div class="col-lg-4">
+            <div class="main-feature">
+               <figure>
+                  <img src="images/level-ii-pricing.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+                  <h4 class="text-primary fw-600">
+                     Level II Pricing
+                  </h4>
+                  <p>
+                     See the market clearly with Level II Pricing, trading directly with liquidity providers to gain insights and potentially benefit from improved pricing.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/algorithmic-trading.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Algorithmic Trading
+	               </h4>
+	               <p>
+	                  Create or use algorithmic trading strategies with cTrader's user-friendly platform, allowing automated trading based on preset rules and conditions.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/advanced-charting-and-analysis.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Advanced Charting and Analysis
+	               </h4>
+	               <p>
+	                  Leverage advanced charting, a variety of technical indicators, and drawing tools on cTrader for detailed market analysis, aiding in making well-informed trading decisions.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/fast-order-execution.png" class="img-fluid">
+               </figure>
+               <h4 class="text-primary fw-600">
+                  Fast Order Execution
+               </h4>
+               <p>
+                  Experience rapid order execution on cTrader, ensuring your trades are executed promptly, minimizing slippage, and enhancing trade precision.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+            	<figure>
+            		<img src="images/multiple-order-types.png" class="img-fluid">
+            	</figure>
+               <h4 class="text-primary fw-600">
+                  Various Order Types
+               </h4>
+               <p>
+                  Utilise multiple order types, including market orders, limit orders, stop orders, and OCO (One Cancels the Other) orders, providing flexibility and control over your trading strategies.
+               </p>
+            </div>
+         </div>
+      </div>
+      <div class="row">
+      	<div class="col-md-12">
+      		<div class="text-center mt-5">
+      			<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+      		</div>
+      	</div>
+      </div>
+	</div>
+</section>
+
+<!-- Trading platform CTrader -->
+
+<!-- Why NXG Markets? -->
 <?php include_once('./include/why-nxg-markets.php') ?>
+<!-- Why NXG Markets? -->
 
 <!-- Looking for More? -->
 <section class="trade-section">
