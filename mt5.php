cd1138b83702e4639db60d35796471330084830b
amitfxcareers <amit@fxcareers.com>
2024-05-28 12:08:39 +0530

    MT5 Page done

--- mt5.php
+++ mt5.php
@@ -1,73 +1,208 @@
 <?php include_once('./include/header.php') ?>
 <!-- Banner -->
 <section class="inner-banner" style="background-image: url(images/mt5_banner.webp);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="banner_content ">
-					<h2 class="text-white">
-						Meta Trader 5
-					</h2>
-					<p class="text-white">
-						Explore an upgraded trading experience with over 100 assets on MetaTrader 5 – The Next-Gen Trading Platform.
-					</p>
-					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  Meta Trader 5
+               </h2>
+               <p class="text-white">
+                  Explore an upgraded trading experience with over 100 assets on MetaTrader 5 – The Next-Gen Trading Platform.
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Banner -->
-
+<!-- Trading Platform -->
+<section class="trading-platform">
+   <div class="container">
+      <div class="row gy-4">
+         <div class="col-md-12">
+            <p class="text-center platform-intro">
+               Take your trading to the next level with MT5 at NXG Markets. Benefit from enhanced features, including additional charting tools and economic calendar integration. Access MT5 on desktop, web, and mobile devices to leverage its advanced capabilities. Register now and elevate your trading with MT5.
+            </p>
+         </div>
+      </div>
+      <div class="row mt-4">
+         <div class="col-md-12 text-center">
+            <h5 class="text-black fw-600">
+               Trading Platform
+            </h5>
+            <h2 class="text-primary fw-800">
+               Advantages of Using<br>Meta Trader 5
+            </h2>
+         </div>
+      </div>
+      <div class="row mt-4">
+         <div class="col-md-3">
+            <div class="platform_links">
+               <a href="#">
+                  <i class='fab fa-app-store'></i>
+                  <h4 class="mt-2">App Store</h4>
+               </a>
+            </div>
+         </div>
+         <div class="col-md-3">
+            <div class="platform_links">
+               <a href="#">
+                  <i class="fab fa-android" aria-hidden="true"></i>
+                  <h4 class="mt-2">APK File</h4>
+               </a>
+            </div>
+         </div>
+         <div class="col-md-3">
+            <div class="platform_links">
+               <a href="#">
+                  <i class='fab fa-app-store'></i>
+                  <h4 class="mt-2">Web</h4>
+               </a>
+            </div>
+         </div>
+         <div class="col-md-3">
+            <div class="platform_links">
+               <a href="https://download.mql5.com/cdn/web/nxg.markets.limited/mt5/nxgmarkets5setup.exe">
+                  <i class='fab fa-windows'></i>
+                  <h4 class="mt-2">Windows</h4>
+               </a>
+            </div>
+         </div>
+      </div>
+      <div class="row mt-5">
+         <div class="col-md-12 text-center">
+            <img src="images/metatrader5.png" class="img-fluid" width="60%">
+         </div>
+      </div>
+      <div class="row mt-5 gy-4 justify-content-center text-center">
+         <div class="col-lg-4">
+            <div class="main-feature">
+               <figure>
+                  <img src="images/enhanced-features.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+                  <h4 class="text-primary fw-600">
+                     Enhanced Features
+                  </h4>
+                  <p>
+                     Upgrade your trading with added features like advanced charting tools, economic calendar integration, and Level II market data.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/diverse-asset-classes.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Diverse Asset Classes
+	               </h4>
+	               <p>
+	                  Trade various financial instruments—forex, stocks, commodities, and indices—all within the MT5 platform for diversified trading opportunities.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/economic-calendar-integration.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Economic Calendar Integration
+	               </h4>
+	               <p>
+	                  Stay informed about crucial economic events using MT5's integrated calendar, assisting in making well-informed trading decisions based on upcoming releases.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/versatile-order-types.png" class="img-fluid">
+               </figure>
+               <h4 class="text-primary fw-600">
+                  Versatile Order Types
+               </h4>
+               <p>
+                  Experience flexibility with a range of order types—market orders, pending orders, stop orders, and trailing stops—giving you more control over your trading strategies.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+            	<figure>
+            		<img src="images/mobile-trading.webp" class="img-fluid">
+            	</figure>
+               <h4 class="text-primary fw-600">
+                  Mobile Trading
+               </h4>
+               <p>
+                  Access MT5 on your mobile devices for on-the-go market monitoring, trade execution, and position management, ensuring you never miss a trading opportunity.
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+   </div>
+</section>
+<!-- Trading Platform -->
+<!-- Why NXG Markets -->
 <?php include_once('./include/why-nxg-markets.php') ?>
-
+<!-- Why NXG Markets -->
 <!-- Looking for More? -->
 <section class="trade-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2 class="main-heading">Looking for More?</h2>
-			</div>
-			<div class="col-md-12 mt-4">
-				<div class="trade-menu">
-					<ul class="trade-list-ul">
-						<li>
-							<div class="image-boxx text-center">
-								<a href="mt5.php">
-									<span class="active">
-										<img src="images/MT5.webp" class="img-fluid">
-									</span>
-									<h4>Meta Trader 5</h4>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="image-boxx text-center">
-								<a href="ctrader.php">
-									<span>
-										<img src="images/cTrader.webp" class="img-fluid">
-									</span>
-									<h4>cTrader</h4>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="image-boxx text-center">
-								<a href="copytrading.php">
-									<span>
-										<img src="images/copy-trading.webp" class="img-fluid">
-									</span>
-									<h4>CopyTrading</h4>
-								</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="main-heading">Looking for More?</h2>
+         </div>
+         <div class="col-md-12 mt-4">
+            <div class="trade-menu">
+               <ul class="trade-list-ul">
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="mt5.php">
+                           <span class="active">
+                           <img src="images/MT5.webp" class="img-fluid">
+                           </span>
+                           <h4>Meta Trader 5</h4>
+                        </a>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="ctrader.php">
+                           <span>
+                           <img src="images/cTrader.webp" class="img-fluid">
+                           </span>
+                           <h4>cTrader</h4>
+                        </a>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="image-boxx text-center">
+                        <a href="copytrading.php">
+                           <span>
+                           <img src="images/copy-trading.webp" class="img-fluid">
+                           </span>
+                           <h4>CopyTrading</h4>
+                        </a>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Looking for More? -->
-
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
