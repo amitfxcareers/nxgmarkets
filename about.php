e906228140a101307122a6afda5de84d035a0ea8
amitfxcareers <amit@fxcareers.com>
2024-05-29 12:37:48 +0530

    About NXG and Powered By Trading section added to ABout page

--- about.php
+++ about.php
@@ -83,6 +83,69 @@
 </section>
 <!-- Banner Factoids -->
 
+<!-- ABOUT NXG -->
+<section class="about-nxg">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h5 class="text-black fw-600">
+          About NXG Markets
+        </h5>
+        <h2 class="text-primary fw-800">
+          Who We Are
+        </h2>
+        <p>
+          Welcome to NXG Markets, a leading player in the Forex and CFD markets. As a reliable and principled trading platform, we are committed to establishing a user-friendly and conflict-free trading atmosphere. Since our establishment, we’ve dedicated ourselves to providing educational resources and a practical trading platform covering various assets, including forex, indices, commodities, and cryptocurrencies.
+        </p>
+        <p>
+          At NXG Markets, we prioritize our customer’s needs, offering comprehensive training, outstanding 24-hour customer service, top-tier trade execution, rapid withdrawal systems, and dedicated trading support and account management for traders of all experience levels. Join NXG Markets and enjoy our unwavering commitment to improving your trading journey.
+        </p>
+      </div>
+      <div class="col-md-6">
+        <lottie-player src="images/lottie-json/about-us.json" style="width: 100%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ABOUT NXG -->
+
+<!-- Powered by Trading -->
+<section class="powered_by_trading">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="images/about-nxg.png" class="img-fluid">
+      </div>
+      <div class="col-md-6">
+        <h2 class="text-primary fw-800">
+          Powered By Trading,<br>Dedicated To You.
+        </h2>
+        <h3>
+          We are one of the most trusted and reputable forex trading brokers with strong foundations.
+        </h3>
+        <ol>
+          <li>
+            Exceptional 24-hour customer service to meet your needs.
+          </li>
+          <li>
+            Market-leading trade execution for optimal trading experiences.
+          </li>
+          <li>
+            Fast and efficient withdrawal system.
+          </li>
+          <li>
+            Trading support and account relationship managers for traders at all levels.
+          </li>
+        </ol>
+        <div class="get-in-touch mt-5">
+          <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Powered by Trading -->
+
 
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
