29ce241c550e7f57e567ca3e53106ea6cdc533e3
amitfxcareers <amit@fxcareers.com>
2024-05-29 11:50:27 +0530

    Disclaimer Page done

--- about.php
+++ about.php
@@ -18,7 +18,70 @@
 	</div>
 </section>
 <!-- Banner -->
-
+<!-- Banner Factoids -->
+<section class="banner-why-factoids">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="banner_factoids">
+          <ul>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  1:1000
+                </h2>
+                <p class="text-white">Flexible Leverage<br>
+                  <strong>Enjoy High Leverage</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  100+
+                </h2>
+                <p class="text-white">Trading Instruments<br>
+                  <strong>Diverse Asset Selection</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  0.0
+                </h2>
+                <p class="text-white">Spreads from<br>
+                  <strong>Lowest Spreads Possible</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  Fast
+                </h2>
+                <p class="text-white">Withdrawals<br>
+                  <strong>Fast and Secure Transactions</strong>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="factoids-image-box">
+                <h2 class="main-heading-white">
+                  Regulated
+                </h2>
+                <p class="text-white">Broker<br>
+                  <strong>Trusted & Regulated Trading</strong>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Banner Factoids -->
 
 
 <?php include_once('./include/why-nxg-markets.php') ?>
