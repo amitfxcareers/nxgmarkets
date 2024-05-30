30e697df3543e01211aed902d75a159a793400cc
amitfxcareers <amit@fxcareers.com>
2024-05-30 14:56:22 +0530

    Contact Us Page Done

--- contact-us.php
+++ contact-us.php
@@ -18,5 +18,228 @@
 	</div>
 </section>
 <!-- Banner -->
+
+<!-- Contact Us -->
+<section class="contact_us">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<p>
+					At NXG Markets, we value your thoughts and queries. Your journey in the financial markets matters to us. That’s why our team is always ready to assist and guide you. Feel free to connect with us regarding any trading concerns, account inquiries or just to share your feedback. Let’s shape your trading experience together. We’re just a message away
+				</p>
+				<h5 class="text-black fw-600">
+					Contact NXG Markets
+				</h5>
+				<h2 class="text-primary fw-800">
+					Get in Touch
+				</h2>
+				<p>
+					Select Your Preferred Communication Method: Your comfort is our priority when it comes to customer service. Whether you favour a swift chat, an email, or a direct phone call, we have all your communication needs covered. Explore the options below to reach out to us in the manner that best suits you. We’re excitedly anticipating your contact.
+				</p>
+			</div>
+			<div class="col-md-6">
+				<lottie-player src="images/lottie-json/contact-us.json" style="width: 100%; height: 500px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-3">
+				<div class="contact_list">
+					<div class="contact-icon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<div class="list-details">
+						<h5 class="fw-800">
+							General Inquiries
+						</h5>
+						<p>
+							info@nxgmarkets.com
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="contact_list">
+					<div class="contact-icon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<div class="list-details">
+						<h5 class="fw-800">
+							Finance Inquiries
+						</h5>
+						<p>
+							finance@nxgmarkets.com
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="contact_list">
+					<div class="contact-icon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<div class="list-details">
+						<h5 class="fw-800">
+							Support
+						</h5>
+						<p>
+							support@nxgmarkets.com
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="contact_list">
+					<div class="contact-icon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<div class="list-details">
+						<h5 class="fw-800">
+							Partner Inquiries
+						</h5>
+						<p>
+							partner@nxgmarkets.com
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Contact Us -->
+
+<!-- Contact Us Form-->
+<section class="contact_us_form">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-8">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<ul>
+								<li>
+									<div class="contact_list">
+										<div class="contact-icon2">
+											<i class="fa-solid fa-location-dot"></i>
+										</div>
+										<div class="list-details">
+											<h5 class="fw-800">
+												Physical Address
+											</h5>
+											<p>
+												#806, AL SERKAL BUILDING 2 PORT SAEED DUBAI U.A.E.
+											</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="contact_list">
+										<div class="contact-icon2">
+											<i class="fa-solid fa-location-dot"></i>
+										</div>
+										<div class="list-details">
+											<h5 class="fw-800">
+												Registered Address
+											</h5>
+											<p>
+												BONOVO ROAD – FOMBONI ISLAND OF MOHELI – COMOROS UNION
+											</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="contact_list">
+										<div class="contact-icon2">
+											<i class="fa-solid fa-location-dot"></i>
+										</div>
+										<div class="list-details">
+											<h5 class="fw-800">
+												Principal Business Address
+											</h5>
+											<p>
+												L36 1 MACQUARIE PL SYDNEY NSW 2000
+											</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="contact_list">
+										<div class="contact-icon2">
+											<i class="fa fa-phone" aria-hidden="true"></i>
+										</div>
+										<div class="list-details">
+											<h5 class="fw-800">
+												Call Us
+											</h5>
+											<p>
+												+043432219
+											</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="contact_list">
+										<div class="contact-icon2">
+											<i class="fa-brands fa-whatsapp"></i>
+										</div>
+										<div class="list-details">
+											<h5 class="fw-800">
+												Message Us
+											</h5>
+											<p>
+												+61-480-037-361
+											</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="contact_list">
+										<div class="contact-icon2">
+											<i class="fa fa-envelope" aria-hidden="true"></i>
+										</div>
+										<div class="list-details">
+											<h5 class="fw-800">
+												Email Address
+											</h5>
+											<p>
+												info@nxgmarkets.com
+											</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="contact-form">
+					<h2 class="mb-2 text-primary">Send us a message</h2>
+					<form method="POST" action="#" enctype="multipart/form-data" class="form-group mt-3 mt-sm-4 mb-0">
+						<div class="row">
+							<div class="col-12 mb-3">
+								<input class="form-control form-item" type="text" name="name" placeholder="Name" required="">
+							</div>
+							<div class="col-12 mb-3">
+								<input class="form-control form-item" type="email" name="email" placeholder="Email" required="">
+							</div>
+							<div class="col-12 mb-3">
+								<input class="form-control form-item" type="text" name="phone_number" placeholder="Mobile" required="">
+							</div>
+							<div class="col-12 mb-3">
+								<textarea class="form-control form-item" name="message" placeholder="Message" required=""></textarea>
+							</div> 
+							<div class="col-12">
+								<button class="theme-btn theme-btn2 mt-3" name="submit">Submit Now</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Contact Us Form-->
+
+
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
