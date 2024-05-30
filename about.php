de593749d1f29b689f0295e5a02be6fe7d14a586
amitfxcareers <amit@fxcareers.com>
2024-05-30 12:05:31 +0530

    About Page done

--- about.php
+++ about.php
@@ -1,182 +1,189 @@
 <?php include_once('./include/header.php') ?>
 <!-- Banner -->
 <section class="inner-banner" style="background-image: url(images/about-banner.png);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="banner_content ">
-					<h2 class="text-white">
-						About Us
-					</h2>
-					<p class="text-white">
-						Welcome to NXG Markets – Your Trading Companion for Precision Trading and Proven Results.
-					</p>
-					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  About Us
+               </h2>
+               <p class="text-white">
+                  Welcome to NXG Markets – Your Trading Companion for Precision Trading and Proven Results.
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Banner -->
 <!-- Banner Factoids -->
 <section class="banner-why-factoids">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="banner_factoids">
-          <ul>
-            <li>
-              <div class="factoids-image-box">
-                <h2 class="main-heading-white">
-                  1:1000
-                </h2>
-                <p class="text-white">Flexible Leverage<br>
-                  <strong>Enjoy High Leverage</strong>
-                </p>
-              </div>
-            </li>
-            <li>
-              <div class="factoids-image-box">
-                <h2 class="main-heading-white">
-                  100+
-                </h2>
-                <p class="text-white">Trading Instruments<br>
-                  <strong>Diverse Asset Selection</strong>
-                </p>
-              </div>
-            </li>
-            <li>
-              <div class="factoids-image-box">
-                <h2 class="main-heading-white">
-                  0.0
-                </h2>
-                <p class="text-white">Spreads from<br>
-                  <strong>Lowest Spreads Possible</strong>
-                </p>
-              </div>
-            </li>
-            <li>
-              <div class="factoids-image-box">
-                <h2 class="main-heading-white">
-                  Fast
-                </h2>
-                <p class="text-white">Withdrawals<br>
-                  <strong>Fast and Secure Transactions</strong>
-                </p>
-              </div>
-            </li>
-            <li>
-              <div class="factoids-image-box">
-                <h2 class="main-heading-white">
-                  Regulated
-                </h2>
-                <p class="text-white">Broker<br>
-                  <strong>Trusted & Regulated Trading</strong>
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="banner_factoids">
+               <ul>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           1:1000
+                        </h2>
+                        <p class="text-white">Flexible Leverage<br>
+                           <strong>Enjoy High Leverage</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           100+
+                        </h2>
+                        <p class="text-white">Trading Instruments<br>
+                           <strong>Diverse Asset Selection</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           0.0
+                        </h2>
+                        <p class="text-white">Spreads from<br>
+                           <strong>Lowest Spreads Possible</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           Fast
+                        </h2>
+                        <p class="text-white">Withdrawals<br>
+                           <strong>Fast and Secure Transactions</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           Regulated
+                        </h2>
+                        <p class="text-white">Broker<br>
+                           <strong>Trusted & Regulated Trading</strong>
+                        </p>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
       </div>
-    </div>
-  </div>
+   </div>
 </section>
 <!-- Banner Factoids -->
-
 <!-- ABOUT NXG -->
 <section class="about-nxg">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <h5 class="text-black fw-600">
-          About NXG Markets
-        </h5>
-        <h2 class="text-primary fw-800">
-          Who We Are
-        </h2>
-        <p>
-          Welcome to NXG Markets, a leading player in the Forex and CFD markets. As a reliable and principled trading platform, we are committed to establishing a user-friendly and conflict-free trading atmosphere. Since our establishment, we’ve dedicated ourselves to providing educational resources and a practical trading platform covering various assets, including forex, indices, commodities, and cryptocurrencies.
-        </p>
-        <p>
-          At NXG Markets, we prioritize our customer’s needs, offering comprehensive training, outstanding 24-hour customer service, top-tier trade execution, rapid withdrawal systems, and dedicated trading support and account management for traders of all experience levels. Join NXG Markets and enjoy our unwavering commitment to improving your trading journey.
-        </p>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <h5 class="text-black fw-600">
+               About NXG Markets
+            </h5>
+            <h2 class="text-primary fw-800">
+               Who We Are
+            </h2>
+            <p>
+               Welcome to NXG Markets, a leading player in the Forex and CFD markets. As a reliable and principled trading platform, we are committed to establishing a user-friendly and conflict-free trading atmosphere. Since our establishment, we’ve dedicated ourselves to providing educational resources and a practical trading platform covering various assets, including forex, indices, commodities, and cryptocurrencies.
+            </p>
+            <p>
+               At NXG Markets, we prioritize our customer’s needs, offering comprehensive training, outstanding 24-hour customer service, top-tier trade execution, rapid withdrawal systems, and dedicated trading support and account management for traders of all experience levels. Join NXG Markets and enjoy our unwavering commitment to improving your trading journey.
+            </p>
+         </div>
+         <div class="col-md-6">
+            <lottie-player src="images/lottie-json/about-us.json" style="width: 100%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
       </div>
-      <div class="col-md-6">
-        <lottie-player src="images/lottie-json/about-us.json" style="width: 100%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
-      </div>
-    </div>
-  </div>
+   </div>
 </section>
 <!-- ABOUT NXG -->
-
 <!-- Powered by Trading -->
 <section class="powered_by_trading">
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-md-6">
-        <img src="images/about-nxg.png" class="img-fluid">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <img src="images/about-nxg.png" class="img-fluid">
+         </div>
+         <div class="col-md-6">
+            <h2 class="text-primary fw-800">
+               Powered By Trading,<br>Dedicated To You.
+            </h2>
+            <h3>
+               We are one of the most trusted and reputable forex trading brokers with strong foundations.
+            </h3>
+            <ol>
+               <li>
+                  Exceptional 24-hour customer service to meet your needs.
+               </li>
+               <li>
+                  Market-leading trade execution for optimal trading experiences.
+               </li>
+               <li>
+                  Fast and efficient withdrawal system.
+               </li>
+               <li>
+                  Trading support and account relationship managers for traders at all levels.
+               </li>
+            </ol>
+            <div class="get-in-touch mt-5">
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+            </div>
+         </div>
       </div>
-      <div class="col-md-6">
-        <h2 class="text-primary fw-800">
-          Powered By Trading,<br>Dedicated To You.
-        </h2>
-        <h3>
-          We are one of the most trusted and reputable forex trading brokers with strong foundations.
-        </h3>
-        <ol>
-          <li>
-            Exceptional 24-hour customer service to meet your needs.
-          </li>
-          <li>
-            Market-leading trade execution for optimal trading experiences.
-          </li>
-          <li>
-            Fast and efficient withdrawal system.
-          </li>
-          <li>
-            Trading support and account relationship managers for traders at all levels.
-          </li>
-        </ol>
-        <div class="get-in-touch mt-5">
-          <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
-        </div>
-      </div>
-    </div>
-  </div>
+   </div>
 </section>
 <!-- Powered by Trading -->
-
 <!-- What Makes Us NXG Markets -->
 <section class="what-makes-nxg">
-  <div class="container">
-    <div class="row gy-4">
-      <div class="col-md-12">
-        <h2 class="text-primary fw-800">
-          What Makes Us NXG Markets?
-        </h2>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="text-primary fw-800">
+               What Makes Us NXG Markets?
+            </h2>
+         </div>
       </div>
-      <div class="col-md-6">
-        <div class="mission-content">
-          <h3 class="text-black fw-800">
-            Our Mission
-          </h3>
-          <p>
-            At NXG Markets, our mission is to equip traders worldwide with the knowledge, tools, and support needed for success. Through a dependable and ethical trading platform, we deliver outstanding customer service, innovative technologies, and comprehensive resources to assist traders in reaching their financial goals.
-          </p>
-        </div>
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <div class="mission-content">
+               <h3 class="text-black fw-800">
+                  Our Mission
+               </h3>
+               <p>
+                  At NXG Markets, our mission is to equip traders worldwide with the knowledge, tools, and support needed for success. Through a dependable and ethical trading platform, we deliver outstanding customer service, innovative technologies, and comprehensive resources to assist traders in reaching their financial goals.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-6">
+            <lottie-player src="images/lottie-json/our-mission.json" style="width: 100%; height: 400px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
       </div>
-      <div class="col-md-6">
-        <div class="mission-content">
-          <h3 class="text-black fw-800">
-            Our Vision
-          </h3>
-          <p>
-            Our Vision is to be the preferred choice for traders seeking a reliable and transparent trading experience globally. Recognized for exceptional trading conditions, advanced technologies, and unwavering dedication to customer satisfaction, NXG Markets aims to empower individuals to confidently navigate the financial markets.
-          </p>
-        </div>
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <lottie-player src="images/lottie-json/our-vision.json" style="width: 100%; height: 400px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
+         <div class="col-md-6">
+            <div class="mission-content">
+               <h3 class="text-black fw-800">
+                  Our Vision
+               </h3>
+               <p>
+                  Our Vision is to be the preferred choice for traders seeking a reliable and transparent trading experience globally. Recognized for exceptional trading conditions, advanced technologies, and unwavering dedication to customer satisfaction, NXG Markets aims to empower individuals to confidently navigate the financial markets.
+               </p>
+            </div>
+         </div>
       </div>
-    </div>
-    <div class="row mt-5 gy-4 justify-content-center text-center">
+      <div class="row mt-5 gy-4 justify-content-center text-center">
          <div class="col-lg-4">
             <div class="main-feature">
                <figure>
@@ -249,10 +256,9 @@
             </div>
          </div>
       </div>
-  </div>
+   </div>
 </section>
 <!-- What Makes Us NXG Markets -->
-
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
