ba9d289a5db21ee7069d2c98f8d2a331ad239afe
amitfxcareers <amit@fxcareers.com>
2024-05-30 17:45:39 +0530

    NXG Affiliate Page Started

--- nxgmarketsaffiliates.php
+++ nxgmarketsaffiliates.php
@@ -19,5 +19,46 @@
 </section>
 <!-- Banner -->
 
+<!-- NXG Markets Affiliates -->
+<section class="nxg-affiliates">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<p>
+					Experience the synergy of collaborating with NXG Markets, a distinguished Forex broker. Leverage our platform’s reputation and comprehensive suite of trading products to diversify your income stream.
+				</p>
+				<h5 class="text-black fw-600">
+					Start Earning Today
+				</h5>
+				<h2 class="text-primary fw-800">
+					About NXG Markets Affiliates
+				</h2>
+				<p>
+					Join forces with NXG Markets as an Affiliate and tap into new revenue opportunities in the world of Forex trading. We’ve built our reputation on trust, reliability, and exceptional service, making NXG Markets an ideal partner for those seeking to monetize their network.
+				</p>
+				<p>
+					As an Affiliate, you will benefit from competitive commissions, marketing tools, and dedicated support, all aimed at maximizing your potential.
+				</p>
+			</div>
+			<div class="col-md-6">
+            <lottie-player src="images/lottie-json/affiliate.json" style="width: 100%; height: 400px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
+		</div>
+	</div>
+	<div class="container nxg-affiliates-two">
+		<div class="row">
+			<div class="col-md-6">
+				<h5 class="text-white fw-600">
+					Affiliate Payout Terms
+				</h5>
+				<h2 class="text-white fw-800">
+					Our Transparent Commission Structure
+				</h2>
+			</div>
+			<div class="col-md-6"></div>
+		</div>
+	</div>
+</section>
+<!-- NXG Markets Affiliates -->
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/footer.php') ?>
