0fddf976d72d8eee2cfcd39bbdf9dc9863e17c1f
amitfxcareers <amit@fxcareers.com>
2024-06-01 18:39:03 +0530

    banner factoids added

--- advantages.php
+++ advantages.php
@@ -18,5 +18,238 @@
 	</div>
 </section>
 <!-- Banner -->
+
+<!-- Banner Factoids -->
+<section class="banner-why-factoids">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="banner_factoids">
+               <ul>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           1:1000
+                        </h2>
+                        <p class="text-white">Flexible Leverage<br>
+                           <strong>Enjoy High Leverage</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           100+
+                        </h2>
+                        <p class="text-white">Trading Instruments<br>
+                           <strong>Diverse Asset Selection</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           0.0
+                        </h2>
+                        <p class="text-white">Spreads from<br>
+                           <strong>Lowest Spreads Possible</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           Fast
+                        </h2>
+                        <p class="text-white">Withdrawals<br>
+                           <strong>Fast and Secure Transactions</strong>
+                        </p>
+                     </div>
+                  </li>
+                  <li>
+                     <div class="factoids-image-box">
+                        <h2 class="main-heading-white">
+                           Regulated
+                        </h2>
+                        <p class="text-white">Broker<br>
+                           <strong>Trusted & Regulated Trading</strong>
+                        </p>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Banner Factoids -->
+
+<!-- Advantages -->
+<section class="advantages-section">
+	<div class="container">
+		<div class="row mt-5 gy-4 justify-content-center text-center">
+         <div class="col-lg-4">
+            <div class="main-feature">
+               <figure>
+                  <img src="images/enhanced-features.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+                  <h4 class="text-primary fw-600">
+                     Earn Up To $25 Per Lot For Clients You Refer
+                  </h4>
+                  <p>
+                     NXG Markets values your efforts and rewards you generously through our Partner Program, which boasts highly competitive commission rates.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/diverse-asset-classes.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Earn an Unlimited 10% on Your Sub-Partners
+	               </h4>
+	               <p>
+	                  Introduce another partner to NXG Markets, and they automatically become your sub-partner, allowing you to earn an unlimited 10% from their activities.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/economic-calendar-integration.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  No Limit on Commissions Per Client
+	               </h4>
+	               <p>
+	                  Our Partner Program offers limitless earning potential, with no restrictions on the commissions you can earn per client.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/versatile-order-types.png" class="img-fluid">
+               </figure>
+               <h4 class="text-primary fw-600">
+                  Fully Automated Auto-Rebate System
+               </h4>
+               <p>
+                  Experience an additional effective and fully transparent business method through our Partner Program's automated auto-rebate system, attracting clients seamlessly.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+            	<figure>
+            		<img src="images/mobile-trading.webp" class="img-fluid">
+            	</figure>
+               <h4 class="text-primary fw-600">
+                  Competitive Conversions and High Client Retention
+               </h4>
+               <p>
+                  Benefit from competitive commissions and high overall client retention, showcasing the strength of our Partner Program in consistently retaining partners and clients.
+               </p>
+            </div>
+         </div>
+         <div class="col-lg-4">
+            <div class="main-feature">
+               <figure>
+                  <img src="images/enhanced-features.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+                  <h4 class="text-primary fw-600">
+                     No Limit on Weekly Earnings
+                  </h4>
+                  <p>
+                     Enjoy the concept of limitless earning potential without any upper limit on the amount of commissions you can receive each week.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/diverse-asset-classes.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Get Paid on Time
+	               </h4>
+	               <p>
+	                  Timely payment of your earnings is a cornerstone of the NXG Markets Partner Program, reflecting our commitment to fast and reliable withdrawals.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/economic-calendar-integration.png" class="img-fluid">
+               </figure>
+               <div class="feature-content">
+	               	<h4 class="text-primary fw-600">
+	                  Personal Account Manager in Your Language
+	               </h4>
+	               <p>
+	                  Our multilingual customer service team ensures personalized support in 18 languages, providing comfort and assistance to both you and your clients.
+	               </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+               <figure>
+               		<img src="images/versatile-order-types.png" class="img-fluid">
+               </figure>
+               <h4 class="text-primary fw-600">
+                  Exclusive Contests
+               </h4>
+               <p>
+                  Participate in exclusive and ongoing contests tailored specifically for our partners, adding an extra layer of excitement to your experience.
+               </p>
+            </div>
+         </div>
+         <div class="col-md-6 col-lg-4">
+            <div class="main-feature">
+            	<figure>
+            		<img src="images/mobile-trading.webp" class="img-fluid">
+            	</figure>
+               <h4 class="text-primary fw-600">
+                  Exclusive Promotions & Luxury Gifts
+               </h4>
+               <p>
+                  NXG Markets offers a variety of ongoing promotions and bonus schemes to attract new clients, providing opportunities for increased conversions and benefits for partners.
+               </p>
+            </div>
+         </div>
+      </div>
+	</div>
+</section>
+<!-- Advantages -->
+
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/footer.php') ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- nxgmarkets-ibs.php
+++ nxgmarkets-ibs.php
@@ -19,5 +19,58 @@
 </section>
 <!-- Banner -->
 
+<!-- NXG Markets Affiliates -->
+<section class="nxg-affiliates">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<p>
+					At NXG Markets, we appreciate partnerships and collaborations that drive mutual growth. Our Introducing Broker (IB) program is designed to benefit financial firms and professionals aiming to grow their network and boost their income.
+				</p>
+				<h5 class="text-black fw-600">
+					Partner with NXG Markets Today
+				</h5>
+				<h2 class="text-primary fw-800">
+					About Introducing Brokers
+				</h2>
+				<p>
+					NXG Markets’ IB program offers a unique opportunity for individuals and companies to build a profitable partnership. As our IB, you get rewarded for introducing new traders to our platform.
+				</p>
+				<p>
+					We provide full support, personalized service, and lucrative payment terms, ensuring an easy, seamless, and rewarding journey.
+				</p>
+			</div>
+			<div class="col-md-6">
+            <lottie-player src="images/lottie-json/affiliate.json" style="width: 100%; height: 400px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
+		</div>
+	</div>
+	<div class="container nxg-affiliates-two">
+		<div class="row">
+			<div class="col-md-6">
+				<h5 class="text-white fw-600">
+					IB Payout Terms
+				</h5>
+				<h2 class="text-white fw-800">
+					How We Reward Our Introducing Brokers
+				</h2>
+				<p class="text-white">
+					Earnings are as transparent as they are rewarding. With NXG Markets, our Affiliates enjoy competitive, volume-based commissions, paid out monthly. What’s more, we offer detailed performance tracking and reporting tools to help you keep tabs on your success.
+				</p>
+				<!-- <img src="" class="img-fluid"> -->
+			</div>
+			<div class="col-md-6 tc">
+				<div class="transparent_commision">
+					
+				</div>
+				<div class="get-in-touch mt-5">
+	               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+	            </div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- NXG Markets Affiliates -->
+
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/footer.php') ?>
