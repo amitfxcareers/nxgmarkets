1b01de340b8639aa7585e9abbb388008755e024f
amitfxcareers <amit@fxcareers.com>
2024-06-04 11:18:05 +0530

    Blog Listing Page done

--- blog.php
+++ blog.php
@@ -1,3 +1,153 @@
 <?php include_once('./include/header.php') ?>
+<!-- Banner -->
+<section class="inner-banner" style="background-image: url(images/about-banner.png);">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  Blog
+               </h2>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Banner -->
+
+<!-- Blog List -->
+<section class="blog-listing">
+	<div class="container">
+		<div class="row gy-4">
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="blog_list">
+					<a href="blog-detail.php">
+						<img src="images/nxg-blog-8.jpg" class="img-fluid">
+					</a>
+					<div class="blog-content">
+						<a href="blog-detail.php">
+							<h5 class="text-black fw-600">
+								Crypto and Forex Trading with NXG Markets: Get Powerful Tools & Expert Support
+							</h5>
+						</a>
+						<hr>
+						<div class="published_date">
+							<i class="fa fa-calendar" aria-hidden="true"></i> May 23, 2024
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Blog List -->
 
 <?php include_once('./include/footer.php') ?>
