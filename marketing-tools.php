2af78bfe67c333f48f5bfd4cb9c8f81b068dc0c2
amitfxcareers <amit@fxcareers.com>
2024-06-04 12:43:10 +0530

    Marketing Tools Page Done

--- marketing-tools.php
+++ marketing-tools.php
@@ -18,4 +18,80 @@
 	</div>
 </section>
 <!-- Banner -->
+
+<!-- Marketing Tools Listing -->
+<section class="marketing-tools">
+	<div class="container">
+		<div class="row gy-4">
+			<div class="col-md-6">
+				<div class="marketing-tools-list">
+					<h4 class="fw-800">
+						Landing Pages
+					</h4>
+					<p>
+						Benefit from our specially designed landing pages to test your campaigns and improve conversion rates.
+					</p>
+					<img src="images/nxg-markets-landing-page.jpeg" class="img-fluid">	
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="marketing-tools-list">
+					<h4 class="fw-800">
+						Flash, Animated and Static Banners
+					</h4>
+					<p>
+						Promote the NXG Markets FX brand using our high-quality HTML5 and static banners.
+					</p>
+					<img src="images/NXG-STATIC-BANNER.jpg" class="img-fluid">	
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="marketing-tools-list">
+					<h4 class="fw-800">
+						Videos
+					</h4>
+					<p>
+						Engage your clients and potential customers with entertaining, informative, and branded NXG Markets videos.
+					</p>
+					<img src="images/NXG-LANDING-PAGE-OF-VIDEO-01-2048x682.jpg" class="img-fluid">	
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="marketing-tools-list">
+					<h4 class="fw-800">
+						Emails
+					</h4>
+					<p>
+						Utilize custom-designed, copywritten emails to send to your clients and boost conversion rates.
+					</p>
+					<img src="images/Mailers.jpg" class="img-fluid">	
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="marketing-tools-list">
+					<h4 class="fw-800">
+						Webinars and Articles
+					</h4>
+					<p>
+						Provide your clients with custom-made webinars, articles, and trading guides to maximize your earning potential.
+					</p>
+					<img src="images/webinarss.jpg" class="img-fluid">	
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="marketing-tools-list">
+					<h4 class="fw-800">
+						Rollup Banners
+					</h4>
+					<p>
+						Ensure a professional look for your events with our diverse range of customizable roll-up banners.
+					</p>
+					<img src="images/rollup-banner.jpeg" class="img-fluid">	
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Marketing Tools Listing -->
+
 <?php include_once('./include/footer.php') ?>
