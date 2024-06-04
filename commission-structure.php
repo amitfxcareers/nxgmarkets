c9d1a9c3b5f496c3efdc5f067e8c26592abd3d58
amitfxcareers <amit@fxcareers.com>
2024-06-04 16:22:55 +0530

    Commission structure Page Done

--- commission-structure.php
+++ commission-structure.php
@@ -1,23 +1,275 @@
 <?php include_once('./include/header.php') ?>
 <!-- Banner -->
 <section class="inner-banner" style="background-image: url(images/advantages-banner.png);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="banner_content ">
-					<h2 class="text-white">
-						Commission structure
-					</h2>
-					<p class="text-white">
-						<!-- Explore our advantages and experience the best opportunities for collaboration, backed by concrete figures. -->
-					</p>
-					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  Commission structure
+               </h2>
+               <p class="text-white">
+                  <!-- Explore our advantages and experience the best opportunities for collaboration, backed by concrete figures. -->
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Banner -->
-
+<!-- Commission Structure Details -->
+<section class="commission-structure">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <nav>
+               <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                  <button class="nav-link active" id="nav-start-tab" data-bs-toggle="tab" data-bs-target="#nav-start" type="button" role="tab" aria-controls="nav-start" aria-selected="true">Start</button>
+                  <button class="nav-link" id="nav-pro-tab" data-bs-toggle="tab" data-bs-target="#nav-pro" type="button" role="tab" aria-controls="nav-pro" aria-selected="false">Pro</button>
+                  <button class="nav-link" id="nav-prime-account-tab" data-bs-toggle="tab" data-bs-target="#nav-prime-account" type="button" role="tab" aria-controls="nav-prime-account" aria-selected="false">Prime Account</button>
+               </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+               <div class="tab-pane fade show active" id="nav-start" role="tabpanel" aria-labelledby="nav-start-tab">
+                  <div class="container">
+                     <div class="row mt-4">
+                        <div class="col-md-6">
+                           <div class="commission-structure-details">
+                              <div class="commission-image">
+                                 <img src="images/euro.svg" class="img-fluid">
+                                 <h4 class="text-primary fw-800 m-0">
+                                    Starter IB
+                                 </h4>
+                              </div>
+                              <div class="commission-table">
+                                 <table>
+                                    <tbody>
+                                       <tr>
+                                          <td><i class="fa fa-usd" aria-hidden="true"></i> Commission</td>
+                                          <td>$10</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 1</td>
+                                          <td>20%</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 2</td>
+                                          <td>10%</td>
+                                       </tr>
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <div class="commission-note">
+                                 <p>
+                                    <b>Note</b>: You will start off as a Starter IB and will be upgraded to VIP IB once you have a minimum of 30 active clients in your commission structure.
+                                 </p>
+                              </div>
+                              <div class="text-center mt-5">
+                                 <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="commission-structure-details">
+                              <div class="commission-image">
+                                 <img src="images/euro.svg" class="img-fluid">
+                                 <h4 class="text-primary fw-800 m-0">
+                                    VIP IB
+                                 </h4>
+                              </div>
+                              <div class="commission-table">
+                                 <table>
+                                    <tbody>
+                                       <tr>
+                                          <td><i class="fa fa-usd" aria-hidden="true"></i> Commission</td>
+                                          <td>$12</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 1</td>
+                                          <td>20%</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 2</td>
+                                          <td>10%</td>
+                                       </tr>
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <div class="commission-note">
+                                 <p>
+                                    <b>Note</b>: You will start off as a Starter IB and will be upgraded to VIP IB once you have a minimum of 30 active clients in your commission structure.
+                                 </p>
+                              </div>
+                              <div class="text-center mt-5">
+                                 <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="tab-pane fade" id="nav-pro" role="tabpanel" aria-labelledby="nav-pro-tab">
+                  <div class="container">
+                     <div class="row mt-4">
+                        <div class="col-md-6">
+                           <div class="commission-structure-details">
+                              <div class="commission-image">
+                                 <img src="images/euro.svg" class="img-fluid">
+                                 <h4 class="text-primary fw-800 m-0">
+                                    Starter IB
+                                 </h4>
+                              </div>
+                              <div class="commission-table">
+                                 <table>
+                                    <tbody>
+                                       <tr>
+                                          <td><i class="fa fa-usd" aria-hidden="true"></i> Commission</td>
+                                          <td>$5</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 1</td>
+                                          <td>20%</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 2</td>
+                                          <td>10%</td>
+                                       </tr>
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <div class="commission-note">
+                                 <p>
+                                    <b>Note</b>: You will start off as a Starter IB and will be upgraded to VIP IB once you have a minimum of 30 active clients in your commission structure.
+                                 </p>
+                              </div>
+                              <div class="text-center mt-5">
+                                 <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="commission-structure-details">
+                              <div class="commission-image">
+                                 <img src="images/euro.svg" class="img-fluid">
+                                 <h4 class="text-primary fw-800 m-0">
+                                    VIP IB
+                                 </h4>
+                              </div>
+                              <div class="commission-table">
+                                 <table>
+                                    <tbody>
+                                       <tr>
+                                          <td><i class="fa fa-usd" aria-hidden="true"></i> Commission</td>
+                                          <td>$7</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 1</td>
+                                          <td>20%</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 2</td>
+                                          <td>10%</td>
+                                       </tr>
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <div class="commission-note">
+                                 <p>
+                                    <b>Note</b>: You will start off as a Starter IB and will be upgraded to VIP IB once you have a minimum of 30 active clients in your commission structure.
+                                 </p>
+                              </div>
+                              <div class="text-center mt-5">
+                                 <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="tab-pane fade" id="nav-prime-account" role="tabpanel" aria-labelledby="nav-prime-account-tab">
+               	<div class="container">
+                     <div class="row mt-4">
+                        <div class="col-md-6">
+                           <div class="commission-structure-details">
+                              <div class="commission-image">
+                                 <img src="images/euro.svg" class="img-fluid">
+                                 <h4 class="text-primary fw-800 m-0">
+                                    Starter IB
+                                 </h4>
+                              </div>
+                              <div class="commission-table">
+                                 <table>
+                                    <tbody>
+                                       <tr>
+                                          <td><i class="fa fa-usd" aria-hidden="true"></i> Commission</td>
+                                          <td>$1</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 1</td>
+                                          <td>20%</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 2</td>
+                                          <td>10%</td>
+                                       </tr>
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <div class="commission-note">
+                                 <p>
+                                    <b>Note</b>: You will start off as a Starter IB and will be upgraded to VIP IB once you have a minimum of 30 active clients in your commission structure.
+                                 </p>
+                              </div>
+                              <div class="text-center mt-5">
+                                 <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="commission-structure-details">
+                              <div class="commission-image">
+                                 <img src="images/euro.svg" class="img-fluid">
+                                 <h4 class="text-primary fw-800 m-0">
+                                    VIP IB
+                                 </h4>
+                              </div>
+                              <div class="commission-table">
+                                 <table>
+                                    <tbody>
+                                       <tr>
+                                          <td><i class="fa fa-usd" aria-hidden="true"></i> Commission</td>
+                                          <td>$2</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 1</td>
+                                          <td>20%</td>
+                                       </tr>
+                                       <tr>
+                                          <td><i class="fa fa-area-chart" aria-hidden="true"></i> Level 2</td>
+                                          <td>10%</td>
+                                       </tr>
+                                    </tbody>
+                                 </table>
+                              </div>
+                              <div class="commission-note">
+                                 <p>
+                                    <b>Note</b>: You will start off as a Starter IB and will be upgraded to VIP IB once you have a minimum of 30 active clients in your commission structure.
+                                 </p>
+                              </div>
+                              <div class="text-center mt-5">
+                                 <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Commission Structure Details -->
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
