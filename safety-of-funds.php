1628fcb7a1f1b639d4b5278b806282652aa9bf5d
amitfxcareers <amit@fxcareers.com>
2024-06-10 12:17:50 +0530

    Working on Safety of Funds Page

--- safety-of-funds.php
+++ safety-of-funds.php
@@ -19,6 +19,83 @@
 </section>
 <!-- Banner -->
 
+<!-- Prioritizing Safety -->
+<section class="safety-of-funds">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+	            <p>
+	                At NXG Markets, we securely hold your funds in segregated accounts with top-tier banks. Our negative balance protection safeguards your initial investment, providing complete peace of mind during trading.
+	            </p>
+	            <h5 class="text-black fw-600">
+	               Fund Safety – Secure Trading at NXG Markets
+	            </h5>
+	            <h2 class="text-primary fw-800">
+	               Prioritizing Safety
+	            </h2>
+	            <p>
+	               At NXG Markets, ensuring the safety of your funds is our utmost priority. We recognize the significance of trust and security in selecting a trading partner. Stringent measures are in place to secure your funds, held in segregated accounts with reputable financial institutions.
+	            </p>
+	            <p>
+	            	Moreover, we utilize advanced encryption technology to protect your personal and financial information. Trade with confidence, knowing that your funds are secure and protected at NXG Markets. Focus on your trades with peace of mind, as we prioritize the security of your funds.
+	            </p>
+	            <div class="mt-4">
+	               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+	            </div>
+			</div>
+			<div class="col-md-6">
+				<lottie-player src="images/lottie-json/fund-safety.json" style="width: 100%; height: 450px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-6">
+        <div class="new-to-forex-trading">
+          <h4 class="text-secondary">
+            Negative Balance Protection
+          </h4>
+          <h2 class="text-primary">
+            Protection Against Market Uncertainties
+          </h2>
+          <p>
+            Engaging in trading within volatile markets presents inherent risks, such as the possibility of a negative balance. At NXG Markets, we offer negative balance protection to shield you from this risk. This ensures that you will stay within your initial investment loss, regardless of market conditions. It’s a crucial feature to seek in a forex broker, showcasing our commitment to prioritising your trading security.
+          </p>
+          <img src="images/protecting-you-from-market.png" class="img-fluid">
+          <div class="request-quote">
+            <a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Learn More</a>
+         </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="new-to-forex-trading">
+          <h4 class="text-secondary">
+            Market Updates
+          </h4>
+          <h2 class="text-primary">
+            Make Informed Trading Decisions
+          </h2>
+          <p>
+            Stay connected with the most recent market news, expert insights, and thorough analysis. Stay well-informed, maintain your competitive edge, and make decisive trading choices through our regularly updated market news section.
+          </p>
+          <img src="images/stay-ahead.webp" class="img-fluid">
+          <div class="request-quote">
+            <a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Learn More</a>
+         </div>
+        </div>
+      </div>
+		</div>
+	</div>
+</section>
+<!-- Prioritizing Safety -->
+
+<!-- Safety of Funds -->
+<!-- <section class="safety-of-funds">
+	<div class="container">
+		<div class="row"></div>
+	</div>
+</section> -->
+
+<!-- Safety of Funds -->
+
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
