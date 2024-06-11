10d6492001eae3544707c2b49e05c9138a7b3061
amitfxcareers <amit@fxcareers.com>
2024-06-11 13:08:21 +0530

    Refer and Shop page done

--- refer-and-shop.php
+++ refer-and-shop.php
@@ -1,3 +1,305 @@
 <?php include_once('./include/header.php') ?>
 
+<section class="refer-and-earn">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h1 class="refer-heading text-white fw-800">
+                    Refer and Shop
+                </h1>
+                <h3 class="text-white fw-800">
+                    Get Amazon Vouchers upto $1000
+                </h3>
+                <p class="text-white">
+                    Refer new clients to NXG Markets, meet deposit & volume targets and get rewarded BIG with up to $1000 in Amazon vouchers. Bring Big to Win Big!
+                </p>
+                <div class="start-trading-button">
+                    <a href="https://my.nxgmarkets.com/auth/register" class="theme-btn"><i class="fas fa-chart-line" aria-hidden="true"></i>Register Now</a>
+                </div>
+            </div>
+            <div class="col-md-6 text-center">
+                <img src="images/refer-and-shop-image.png" class="img-fluid" width="40%">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="why-join-ib">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="refer-heading fw-800">
+                    Grow Your Network, Grow Your Rewards
+                </h2>
+                <p>
+                    Building a strong network is valuable, and with NXG Markets’ Refer & Shop contest, it’s even more rewarding. Introduce your friends to the world of trading and get rewarded with up to $1,000 in Amazon vouchers when they meet simple deposit and trading goals.
+                </p>
+            </div>
+            <div class="col-md-12">
+                <h2 class="fw-800">
+                    Here’s How It Works
+                </h2>
+                <p>
+                    You refer your friends, and when they open an account with NXG Markets, and make a deposit, everyone wins. Your friends gain access to a powerful trading platform packed with features, allowing them to trade various financial instruments. While they explore exciting trading opportunities, you get rewarded with Amazon vouchers you can spend on anything you desire. It’s a fantastic way to expand your network, support your friends’ financial goals, and earn valuable rewards at the same time. Don’t forget to meet the minimum eligibility criteria and aim for the top.
+                </p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-6 text-center">
+                <div class="sidebar-sticky">
+                    <img src="images/stage.png" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h2 class="fw-800">
+                    Eligibility and Reward Structure
+                </h2>
+                <div class="ib-table mt-4">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="images/deposit.png" class="img-fluid" width="10%">Gross Deposit
+                                </td>
+                                <td>
+                                    $10,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/graph-bar.png" class="img-fluid" width="10%">Volume
+                                </td>
+                                <td>
+                                    100 Lots
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/voucher.png" class="img-fluid" width="10%">Amazon Voucher
+                                </td>
+                                <td>
+                                    $150
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="mt-4">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="images/deposit.png" class="img-fluid" width="10%">Gross Deposit
+                                </td>
+                                <td>
+                                    $25,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/graph-bar.png" class="img-fluid" width="10%">Volume
+                                </td>
+                                <td>
+                                    200 Lots
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/voucher.png" class="img-fluid" width="10%">Amazon Voucher
+                                </td>
+                                <td>
+                                    $500
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="mt-4">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="images/deposit.png" class="img-fluid" width="10%">Gross Deposit
+                                </td>
+                                <td>
+                                    $50,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/graph-bar.png" class="img-fluid" width="10%">Volume
+                                </td>
+                                <td>
+                                    300 Lots
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/voucher.png" class="img-fluid" width="10%">Amazon Voucher
+                                </td>
+                                <td>
+                                    $1,000
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="how-to-join">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="fw-800 text-white">
+                    Easy Steps to Get Started
+                </h2>
+                <p class="text-white">
+                    Join the Contest & Start Earning: Easy Steps to Refer & Shop!
+                </p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-3">
+                <div class="join-image-box one text-center">
+                    <img src="images/edit-1.png" class="img-fluid" width="30%">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Register
+                    </h4>
+                    <p class="text-white">
+                        Register with NXG Markets and set up your IB account.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="join-image-box one text-center">
+                    <img src="images/refer-1.png" class="img-fluid" width="30%">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Refer Clients
+                    </h4>
+                    <p class="text-white">
+                        Get clients to register and make deposits in the account.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="join-image-box one text-center">
+                    <img src="images/earning.png" class="img-fluid" width="30%">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Refer Clients
+                    </h4>
+                    <p class="text-white">
+                        Clear the minimum criteria and start earning as an NXG Markets IB.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="join-image-box text-center">
+                    <img src="images/Shop-Enjoy.png" class="img-fluid" width="30%">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Shop & Enjoy
+                    </h4>
+                    <p class="text-white">
+                        Win Amazon voucher & spend on anything you desire.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="start-referring">
+    <div class="container z-1 position-relative">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="text-primary fw-800">
+                    Start Referring & Start Earning
+                </h2>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="start-refer-image-box text-center">
+                    <img src="images/high-commision-icon.png" class="img-fluid">
+                    <h3 class="fw-800 mt-3">
+                        High Commissions
+                    </h3>
+                    <p>
+                        Get high commissions for every client you refer.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="start-refer-image-box text-center">
+                    <img src="images/unlimited-growth-icon.png" class="img-fluid">
+                    <h3 class="fw-800 mt-3">
+                        Unlimited Growth
+                    </h3>
+                    <p>
+                        Grow your client base and earn with every client.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="start-refer-image-box text-center">
+                    <img src="images/01_Account-Set-Up.png" class="img-fluid">
+                    <h3 class="fw-800 mt-3">
+                        Backend Support
+                    </h3>
+                    <p>
+                        Get all the help in setting up your IB setup.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="terms-conditions">
+    <div class="container">
+        <div class="row tnc-box">
+            <div class="col-md-12 text-center">
+                <h2 class="text-primary fw-800">
+                    Terms & Conditions
+                </h2>
+            </div>
+            <div class="col-md-12 mt-3">
+                <ol>
+                    <li>
+                        This Refer & Shop program is valid from June 1st, 2024 to June 30th, 2024.
+                    </li>
+                    <li>
+                        A new active trader is defined as a new registered individual, not having any account with NXG Markets to date.
+                    </li>
+                    <li>
+                        After withdrawal, the total remaining deposit must be at least 50% of the gross deposit amount.
+                    </li>
+                    <li>
+                        To be eligible for a prize, an affiliate must have a minimum of 5 accounts in a month.
+                    </li>
+                    <li>
+                        Reward will be calculated on 1st of June 2024.
+                    </li>
+                    <li>
+                        Reward will be credit on 5th-10th of June 2024.
+                    </li>
+                    <li>
+                        Sub-accounts do not count for “new open account” requirement.
+                    </li>
+                    <li>
+                        Internal transfers between accounts are not eligible for meeting the required “deposit” criteria.
+                    </li>
+                    <li>
+                        NXG Markets reserves the right to change the terms and conditions or cancel the competition at any time without any prior notice.
+                    </li>
+                    <li>
+                        NXG Markets trading conditions apply on all trading accounts referred by the contestants.
+                    </li>
+                    <li>
+                        NXG Markets has the right to discard any trades that are not compliant with the trading conditions from the volume calculation.
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php include_once('./include/footer.php') ?>
