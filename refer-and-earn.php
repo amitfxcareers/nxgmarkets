bedcb3e55cb6995a0bbb7f928817a2f9071c0316
amitfxcareers <amit@fxcareers.com>
2024-06-11 12:23:00 +0530

    Refer and Earn Page done

--- refer-and-earn.php
+++ refer-and-earn.php
@@ -1,3 +1,261 @@
 <?php include_once('./include/header.php') ?>
 
+<section class="refer-and-earn">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h1 class="refer-heading text-white fw-800">
+                    Refer and Earn
+                </h1>
+                <h3 class="text-white fw-800">
+                    NXG Markets IB Contest
+                </h3>
+                <p class="text-white">
+                    Know someone interested in forex trading? Earn when they join! NXG Markets IB contest lets you refer
+                    friends and get rewarded. Build your network, compete in the contests for bonus cash, and become an
+                    expert. Easy signup, great rewards – join the NXG IB Contest now starting 1st June!
+                </p>
+                <div class="start-trading-button">
+                    <a href="https://my.nxgmarkets.com/auth/register" class="theme-btn"><i class="fas fa-chart-line"
+                            aria-hidden="true"></i>Register Now</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="images/image-1.png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="why-join-ib">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="refer-heading fw-800">
+                    Why Join The IB Contest?
+                </h2>
+                <p>
+                    The NXG Markets Introducing Brokers contest is among the most rewarding IB programs in the market.
+                    Through commissions alone, you can earn enough to become the IB Leader. With our IB program, you can
+                    kickstart your own initiative, managing clients and showcasing your entrepreneurial skills. Our
+                    daily, monthly, and quarterly IB Contests provide opportunities for you to demonstrate your
+                    capabilities through your client network and net deposits, earning recognition and additional
+                    rewards. So join the NXG Markets IB contest now and start your journey to financial freedom.
+                </p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <img src="images/refer-and-earn-2.png" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h2 class="fw-800">
+                    Eligibility and Reward Structure
+                </h2>
+                <div class="ib-table mt-4">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="images/group.png" class="img-fluid" width="10%">
+                                    Accounts
+                                </td>
+                                <td>Minimum 5 Accounts</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/hand.png" class="img-fluid" width="10%">Deposit
+                                </td>
+                                <td>
+                                    Minimum Deposit $500
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/stock-2.png" class="img-fluid" width="10%">Volume
+                                </td>
+                                <td>
+                                    Minimum 2 Lots
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/reward.png" class="img-fluid" width="10%">Reward
+                                </td>
+                                <td>
+                                    $20 per account
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="images/calendar.png" class="img-fluid" width="10%">Date
+                                </td>
+                                <td>
+                                    5th April to 30th April
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="how-to-join">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="fw-800 text-white">
+                    How to Join The IB Contest?
+                </h2>
+                <p class="text-white">
+                    Joining is easy just follow these simple steps:
+                </p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="join-image-box one text-center">
+                    <img src="images/edit-1.png" class="img-fluid">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Register
+                    </h4>
+                    <p class="text-white">
+                        Register with NXG Markets and set up your IB account.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="join-image-box one text-center">
+                    <img src="images/refer-1.png" class="img-fluid">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Refer Clients
+                    </h4>
+                    <p class="text-white">
+                        Get clients to register and make deposits in the account.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="join-image-box text-center">
+                    <img src="images/earning.png" class="img-fluid">
+                    <h4 class="text-primary fw-800 mt-3">
+                        Start Earning
+                    </h4>
+                    <p class="text-white">
+                        Clear the minimum criteria and start earning as an NXG Markets IB.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="start-referring">
+    <div class="container z-1 position-relative">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="text-primary fw-800">
+                    Start Referring & Start Earning
+                </h2>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="start-refer-image-box text-center">
+                    <img src="images/high-commision-icon.png" class="img-fluid">
+                    <h3 class="fw-800 mt-3">
+                        High Commissions
+                    </h3>
+                    <p>
+                        Get high commissions for every client you refer.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="start-refer-image-box text-center">
+                    <img src="images/unlimited-growth-icon.png" class="img-fluid">
+                    <h3 class="fw-800 mt-3">
+                        Unlimited Growth
+                    </h3>
+                    <p>
+                        Grow your client base and earn with every client.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="start-refer-image-box text-center">
+                    <img src="images/01_Account-Set-Up.png" class="img-fluid">
+                    <h3 class="fw-800 mt-3">
+                        Backend Support
+                    </h3>
+                    <p>
+                        Get all the help in setting up your IB setup.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="terms-conditions">
+    <div class="container">
+        <div class="row tnc-box">
+            <div class="col-md-12 text-center">
+                <h2 class="text-primary fw-800">
+                    Terms & Conditions
+                </h2>
+            </div>
+            <div class="col-md-12 mt-3">
+                <ol>
+                    <li>
+                        Starts from 01st June to 30th June 2024.
+                    </li>
+                    <li>
+                        A new active trader is defined as a new registered individual, not having any account with NXG Markets to date.
+                    </li>
+                    <li>
+                        Minimum Deposit 500 USD
+                    </li>
+                    <li>
+                        Minimum 2 standard lot to be trade
+                    </li>
+                    <li>
+                        To be eligible for a prize, an affiliate must have a minimum of 5 accounts in a month.
+                    </li>
+                    <li>
+                        Reward will be calculated on 1st of May 2024.
+                    </li>
+                    <li>
+                        Reward will be credit on 5th-10th of May 2024.
+                    </li>
+                    <li>
+                        Reward will be credit on basis of Net Active account.
+                    </li>
+                    <li>
+                        Reward is not available on ECN and Raw account type.
+                    </li>
+                    <li>
+                        Sub-accounts do not count for “new open account” requirement.
+                    </li>
+                    <li>
+                        Internal transfers between accounts are not eligible for meeting the required “deposit” criteria.
+                    </li>
+                    <li>
+                        NXG Markets reserves the right to change the terms and conditions or cancel the competition at any time without any prior notice.
+                    </li>
+                    <li>
+                        NXG Markets trading conditions apply on all trading accounts referred by the contestants.
+                    </li>
+                    <li>
+                        NXG Markets has the right to discard any trades that are not compliant with the trading conditions from the volume calculation.
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php include_once('./include/footer.php') ?>
