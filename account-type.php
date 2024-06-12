511a870f7773e9d3c42cf24286b8a13d1c68cf3b
bhawnafxcareer <bhawna@fxcareers.com>
2024-06-12 15:57:15 +0530

    accont type page

--- account-type.php
+++ account-type.php
@@ -98,236 +98,327 @@
 	<div class="container">
 		<div class="pricing-table">
 			<div class="row">
-				<div class="col-lg-3">
-					<div class="price-wrapper">
-						<div class=" price-head  text-center">
-							<h2 class="text-primary fw-800">Start</h2>
-							<p class="mb-2">Min Deposit from</p>
-							<h3>$25</h3>
+				<ul class="price-table">
+					<li>
+						<div class="price-wrapper">
+							<div class=" price-head  text-center">
+								<h2 class="text-primary fw-800">Start</h2>
+								<p class="mb-2">Min Deposit from</p>
+								<h3>$25</h3>
+							</div>
+							<div class="price-body">
+								<ul>
+									<li>
+										<p>Minimum trading volume</p>
+										<span>0.01</span>
+									</li>
+									<li>
+										<p>Swap free Account</p>
+										<span>Yes</span>
+									</li>
+									<li>
+										<p>Spread starts from</p>
+										<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="At market opening and closing times and prior to announcements, the market spread may widen substantially. Consequently, you must ensure that you have sufficient funds in your account to cover this.">2 pips <i class="fa-solid fa-question"></i></span>
+									</li>
+									<li>
+										<p>Commission</p>
+										<span>No</span>
+
+									</li>
+									<li>
+										<p>Leverage</p>
+										<span>1:500</span>
+
+									</li>
+									<li>
+										<p>Account Currency types</p>
+										<span>USD</span>
+
+									</li>
+									<li>
+										<p>Margin Call</p>
+										<span>100%</span>
+
+									</li>
+									<li>
+										<p>Stop Out</p>
+										<span>80%</span>
+									</li>
+									<li>
+										<p>Order Execution</p>
+										<h5>Instant Execution</h5>
+									</li>
+
+									<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
+								</ul>
+
+							</div>
 						</div>
-						<div class="price-body">
-							<ul>
-								<li>
-									<p>Minimum trading volume</p>
-									<span>0.01</span>
-								</li>
-								<li>
-									<p>Swap free Account</p>
-									<span>Yes</span>
-								</li>
-								<li>
-									<p>Spread starts from</p>
-									<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="This top tooltip is themed via CSS variables.">2 pips <i class="fa-solid fa-question"></i></span>
-								</li>
-								<li>
-									<p>Commission</p>
-									<span>No</span>
+					</li>
+					<li>
+						<div class="price-wrapper">
+							<div class=" price-head  text-center">
+								<h2 class="text-primary fw-800">PRO</h2>
+								<p class="mb-2">Min Deposit from</p>
+								<h3>$1500</h3>
+							</div>
+							<div class="price-body">
+								<ul>
+									<li>
+										<p>Minimum trading volume</p>
+										<span>0.01</span>
+									</li>
+									<li>
+										<p>Swap free Account</p>
+										<span>Yes</span>
+									</li>
+									<li>
+										<p>Spread starts from</p>
+										<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="At market opening and closing times and prior to announcements, the market spread may widen substantially. Consequently, you must ensure that you have sufficient funds in your account to cover this.">1.2 pips <i class="fa-solid fa-question"></i></span>
+									</li>
+									<li>
+										<p>Commission</p>
+										<span>No</span>
 
-								</li>
-								<li>
-									<p>Leverage</p>
-									<span>1:500</span>
+									</li>
+									<li>
+										<p>Leverage</p>
+										<span>1:500</span>
 
-								</li>
-								<li>
-									<p>Account Currency types</p>
-									<span>USD</span>
+									</li>
+									<li>
+										<p>Account Currency types</p>
+										<span>USD</span>
 
-								</li>
-								<li>
-									<p>Margin Call</p>
-									<span>100%</span>
+									</li>
+									<li>
+										<p>Margin Call</p>
+										<span>100%</span>
 
-								</li>
-								<li>
-									<p>Stop Out</p>
-									<span>80%</span>
-								</li>
-								<li>
-									<p>Order Execution</p>
-									<h5>Instant Execution</h5>
-								</li>
+									</li>
+									<li>
+										<p>Stop Out</p>
+										<span>80%</span>
+									</li>
+									<li>
+										<p>Order Execution</p>
+										<h5>Instant Execution</h5>
+									</li>
 
-								<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
-							</ul>
+									<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
+								</ul>
 
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-lg-3">
-					<div class="price-wrapper">
-						<div class=" price-head  text-center">
-							<h2 class="text-primary fw-800">Start</h2>
-							<p class="mb-2">Min Deposit from</p>
-							<h3>$25</h3>
+					</li>
+					<li>
+						<div class="price-wrapper">
+							<div class=" price-head  text-center">
+								<h2 class="text-white fw-800">Prime</h2>
+								<p class="mb-2 text-white">Min Deposit from</p>
+								<h3 class="text-white">$2500</h3>
+							</div>
+							<div class="price-body">
+								<ul>
+									<li>
+										<p class="text-white">Minimum trading volume</p>
+										<span>0.01</span>
+									</li>
+									<li>
+										<p class="text-white">Swap free Account</p>
+										<span>Yes</span>
+									</li>
+									<li>
+										<p class="text-white">Spread starts from</p>
+										<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="At market opening and closing times and prior to announcements, the market spread may widen substantially. Consequently, you must ensure that you have sufficient funds in your account to cover this.">0.6 pips <i class="fa-solid fa-question"></i></span>
+									</li>
+									<li>
+										<p class="text-white">Commission</p>
+										<span>No</span>
+
+									</li>
+									<li>
+										<p class="text-white">Leverage</p>
+										<span>1:500</span>
+
+									</li>
+									<li>
+										<p class="text-white">Account Currency types</p>
+										<span>USD</span>
+
+									</li>
+									<li>
+										<p class="text-white">Margin Call</p>
+										<span>100%</span>
+
+									</li>
+									<li>
+										<p class="text-white">Stop Out</p>
+										<span>80%</span>
+									</li>
+									<li>
+										<p class="text-white">Order Execution</p>
+										<h5>Instant Execution</h5>
+									</li>
+
+									<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn">Open Account</a></li>
+								</ul>
+
+							</div>
 						</div>
-						<div class="price-body">
-							<ul>
-								<li>
-									<p>Minimum trading volume</p>
-									<span>0.01</span>
-								</li>
-								<li>
-									<p>Swap free Account</p>
-									<span>Yes</span>
-								</li>
-								<li>
-									<p>Spread starts from</p>
-									<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="This top tooltip is themed via CSS variables.">2 pips <i class="fa-solid fa-question"></i></span>
-								</li>
-								<li>
-									<p>Commission</p>
-									<span>No</span>
+					</li>
+					<li>
+						<div class="price-wrapper">
+							<div class=" price-head  text-center">
+								<h2 class="text-primary fw-800">ECN</h2>
+								<p class="mb-2">Min Deposit from</p>
+								<h3>$5000</h3>
+							</div>
+							<div class="price-body">
+								<ul>
+									<li>
+										<p>Minimum trading volume</p>
+										<span>0.01</span>
+									</li>
+									<li>
+										<p>Swap free Account</p>
+										<span>Yes</span>
+									</li>
+									<li>
+										<p>Spread starts from</p>
+										<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="At market opening and closing times and prior to announcements, the market spread may widen substantially. Consequently, you must ensure that you have sufficient funds in your account to cover this.">0 pips <i class="fa-solid fa-question"></i></span>
+									</li>
+									<li>
+										<p>Commission</p>
+										<span>No</span>
 
-								</li>
-								<li>
-									<p>Leverage</p>
-									<span>1:500</span>
+									</li>
+									<li>
+										<p>Leverage</p>
+										<span>1:500</span>
 
-								</li>
-								<li>
-									<p>Account Currency types</p>
-									<span>USD</span>
+									</li>
+									<li>
+										<p>Account Currency types</p>
+										<span>USD</span>
 
-								</li>
-								<li>
-									<p>Margin Call</p>
-									<span>100%</span>
+									</li>
+									<li>
+										<p>Margin Call</p>
+										<span>100%</span>
 
-								</li>
-								<li>
-									<p>Stop Out</p>
-									<span>80%</span>
-								</li>
-								<li>
-									<p>Order Execution</p>
-									<h5>Instant Execution</h5>
-								</li>
+									</li>
+									<li>
+										<p>Stop Out</p>
+										<span>80%</span>
+									</li>
+									<li>
+										<p>Order Execution</p>
+										<h5>Instant Execution</h5>
+									</li>
 
-								<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
-							</ul>
+									<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
+								</ul>
 
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-lg-3">
-					<div class="price-wrapper">
-						<div class=" price-head  text-center">
-							<h2 class="text-primary fw-800">Start</h2>
-							<p class="mb-2">Min Deposit from</p>
-							<h3>$25</h3>
-						</div>
-						<div class="price-body">
-							<ul>
-								<li>
-									<p>Minimum trading volume</p>
-									<span>0.01</span>
-								</li>
-								<li>
-									<p>Swap free Account</p>
-									<span>Yes</span>
-								</li>
-								<li>
-									<p>Spread starts from</p>
-									<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="This top tooltip is themed via CSS variables.">2 pips <i class="fa-solid fa-question"></i></span>
-								</li>
-								<li>
-									<p>Commission</p>
-									<span>No</span>
-
-								</li>
-								<li>
-									<p>Leverage</p>
-									<span>1:500</span>
-
-								</li>
-								<li>
-									<p>Account Currency types</p>
-									<span>USD</span>
-
-								</li>
-								<li>
-									<p>Margin Call</p>
-									<span>100%</span>
-
-								</li>
-								<li>
-									<p>Stop Out</p>
-									<span>80%</span>
-								</li>
-								<li>
-									<p>Order Execution</p>
-									<h5>Instant Execution</h5>
-								</li>
-
-								<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
-							</ul>
-
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3">
-					<div class="price-wrapper">
-						<div class=" price-head  text-center">
-							<h2 class="text-primary fw-800">Start</h2>
-							<p class="mb-2">Min Deposit from</p>
-							<h3>$25</h3>
-						</div>
-						<div class="price-body">
-							<ul>
-								<li>
-									<p>Minimum trading volume</p>
-									<span>0.01</span>
-								</li>
-								<li>
-									<p>Swap free Account</p>
-									<span>Yes</span>
-								</li>
-								<li>
-									<p>Spread starts from</p>
-									<span data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-title="This top tooltip is themed via CSS variables.">2 pips <i class="fa-solid fa-question"></i></span>
-								</li>
-								<li>
-									<p>Commission</p>
-									<span>No</span>
-
-								</li>
-								<li>
-									<p>Leverage</p>
-									<span>1:500</span>
-
-								</li>
-								<li>
-									<p>Account Currency types</p>
-									<span>USD</span>
-
-								</li>
-								<li>
-									<p>Margin Call</p>
-									<span>100%</span>
-
-								</li>
-								<li>
-									<p>Stop Out</p>
-									<span>80%</span>
-								</li>
-								<li>
-									<p>Order Execution</p>
-									<h5>Instant Execution</h5>
-								</li>
-
-								<li><a href="https://my.nxgmarkets.com/auth/register" class="theme-btn theme-btn2">Open Account</a></li>
-							</ul>
-
-						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Pricing table section -->
+<section class="multi-language">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<img src="images/01_New-to-Trading.png" class="img-fluid">
+			</div>
+			<div class="col-md-6">
+				<div class="multi-landuage-right">
+					<h4 class="text-white">Free Demo Account</h4>
+					<h2 class="text-white">
+						Just Starting Out in Trading?
+					</h2>
+					<p class="text-white">
+						Refine your forex trading skills with a demo account from NXG Markets. Open a risk-free demo account today to explore our trading platforms, test strategies, and boost your confidence. It’s an ideal chance to learn and enhance your skills without risking actual funds.
+					</p>
+					<div class="get-in-touch">
+						<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-phone" aria-hidden="true"></i>Get In Touch</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-<!-- Pricing table section -->
+<section class="space-section multiple-trading-platforms">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<img src="images/account-type.png" alt="" class="img-fluid">
+			</div>
+			<div class="col-md-6">
+				<h4 class="text-black mb-4">
+					Trading Platforms
+				</h4>
+				<h2 class="text-secondary">
+				Select Your Trading Platform
+				</h2>
+				<p>
+					Enjoy unparalleled trading freedom with NXG Markets. Benefit from top-tier
+					liquidity across leading platforms without any trading limits or volume
+					constraints. Opt for NXG Markets to maximize your trading capabilities.
+				</p>
+			</div>
+		</div>
+		<div class="row g-4 mt-5">
+			<div class="col-md-4">
+				<div class="mt-icon-boxx">
+					<h4 class="text-primary">Meta Trader 5</h4>
+					<div class="d-flex align-items-center justify-content-between">
+						<p class="text-black">Advanced Features, Exceptional Trading</p>
+						<div class="">
+							<a href="mt5.php"> <i class="fa fa-angle-right text-primary" aria-hidden="true"></i>
+							</a>
+						</div>
+					</div>
+				</div>
 
+			</div>
+			<div class="col-md-4">
+				<div class="mt-icon-boxx">
+					<h4 class="text-primary">cTrader</h4>
+					<div class="d-flex align-items-center justify-content-between">
+						<p class="text-black">Intuitive Design, Power-Packed Experience</p>
+						<div class="">
+							<a href="ctrader.php"> <i class="fa fa-angle-right text-primary" aria-hidden="true"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-md-4">
+				<div class="mt-icon-boxx">
+					<h4 class="text-primary">NXG Markets Trader</h4>
+					<div class="d-flex align-items-center justify-content-between">
+						<p class="text-black">Proprietary Platform with a Competitive Edge</p>
+						<a href="#"> <i class="fa fa-angle-right text-primary" aria-hidden="true"></i> </a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-12 text-end">
+				<div class="mt-5 d-flex align-items-center ">
+					<div class="w-100 divi"><span class="divider"></span></div>
+					<div class="getintouch-bt">
+						<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-phone" aria-hidden="true"></i>Get In Touch</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
