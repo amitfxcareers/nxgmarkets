c3b0fe76c7c0766375805aefd4761334c1657fc0
amitfxcareers <amit@fxcareers.com>
2024-06-12 12:33:40 +0530

    KYC Policy Page done

---
+++ kyc-policy.php
@@ -0,0 +1,81 @@
+<?php include_once('./include/header.php') ?>
+
+<!-- Banner -->
+<section class="inner-banner risk" style="background-image: url(images/risk.jpeg);">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+					    KYC Policy
+               </h2>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Banner -->
+
+<section class="kyc-policy">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <p>
+               Know your customer policies have become increasingly important worldwide lately, especially among banks and other financial institutions, in order to prevent identity theft, money laundering, financial fraud and terrorist activity.
+            </p>
+            <p>
+               Know your customer policies have become increasingly important worldwide lately, especially among banks and other financial institutions, in order to prevent identity theft, money laundering, financial fraud and terrorist activity.
+            </p>
+            <h4 class="fw-800">
+               Prevention:
+            </h4>
+            <p>
+               NXG Markets aims to ensure the integrity of any sensitive data it obtains, such as your account information and the transactions you make, using a variety of security measures and fraud controls. Securing your electronic transactions requires us to be provided with certain data from you, including your preferred deposit method.
+            </p>
+            <h4 class="fw-800">
+               When you deposit funds we will require the following documents:
+            </h4>
+            <ol>
+               <li>
+                  A copy of your valid passport with the signature page
+               </li>
+               <li>
+                  Copies of your credit cards, used to make the deposit (Front side with only the 4 last digits visible, back side with the CVV covered).
+               </li>
+               <li>
+                  A copy of a recent utility bill in your name and address
+               </li>
+               <li>
+                  A signed purchase history of your online transactions
+               </li>
+               <li>
+                  If you have any questions please don’t hesitate to contact our customer support: support@nxgmarkets.com
+               </li>
+            </ol>
+            <h4 class="fw-800">
+               When do I need to provide these documents?
+            </h4>
+            <p>
+               We highly appreciate you taking the time to provide us with all the necessary documents as soon as you can, in order to avoid any delays in processing your transactions. We require the receipt of all the necessary documents prior to making any cash transactions to your benefit.
+            </p>
+            <p>
+               Some circumstances may require us to request these documents before allowing any other activities in your account, such as deposits or trades.
+            </p>
+            <p>
+               Please note that if we will not receive the required documents on file, your pending withdrawals will be cancelled and credited back to your trading account. We will notify you on such event via our system.
+            </p>
+            <h4 class="fw-800">
+               How do I know my documents are safe with you?
+            </h4>
+            <p>
+               NXG Markets holds the security of documentation at highest priority, and treats all documents it receives with utmost respect and confidentiality. All files we obtain are fully protected using the highest level possible of encryption at every step of the review process.
+            </p>
+            <p>
+               We thank you for your cooperation in helping us make NXG Markets a safer place to trade.
+            </p>
+         </div>
+      </div>
+   </div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
