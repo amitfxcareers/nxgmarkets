95ec112c883009c000fae0f5b94486e5838eee50
amitfxcareers <amit@fxcareers.com>
2024-06-12 10:45:27 +0530

    risk disclosure page done

--- risk-disclosure.php
+++ risk-disclosure.php
@@ -1,3 +1,43 @@
 <?php include_once('./include/header.php') ?>
 
+<!-- Banner -->
+<section class="inner-banner risk" style="background-image: url(images/risk.jpeg);">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+					Risk Disclosure
+               </h2>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Banner -->
+
+<section class="risk-disclosure">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p>
+					Risk statement: An investment in derivatives may mean investors may lose an amount even greater than their original investment. Anyone wishing to invest in any of the products mentioned in https://nxgmarkets.com/ should seek their own financial or professional advice. Trading of securities, forex, stock market, commodities, options and futures may not be suitable for everyone and involves the risk of losing part or all of your money. Trading in the financial markets has large potential rewards, but also large potential risk. You must be aware of the risks and be willing to accept them in order to invest in the markets. Don’t invest and trade with money which you can’t afford to lose. Forex Trading are not allowed in some countries, before investing your money, make sure whether your country is allowing this or not.
+				</p>
+				<p>
+					You are strongly advised to obtain independent financial, legal and tax advice before proceeding with any currency or spot metals trade. Nothing in this site should be read or construed as constituting advice on the part of NXG Markets Limited or any of its affiliates, directors, officers or employees.
+				</p>
+				<p>
+					Restricted Regions: NXG Markets Limited does not provide services for citizens/residents of the United States, Cuba, Iraq, Myanmar, North Korea, Sudan, Russia. The services of NXG Markets Limited are not intended for distribution to, or use by, any person in any country or jurisdiction where such distribution or use would be contrary to local law or regulation.
+				</p>
+				<p>OR</p>
+				<p>
+					<i>
+						Information on this site is not directed at residents in any country or jurisdiction where such distribution or use would be contrary to local law or regulation.
+					</i>
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include_once('./include/footer.php') ?>
