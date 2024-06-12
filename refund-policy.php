3b94ac15eb23f61b500965120285c7abaf5b2fc9
amitfxcareers <amit@fxcareers.com>
2024-06-12 12:28:00 +0530

    Refund Page done

---
+++ refund-policy.php
@@ -0,0 +1,78 @@
+<?php include_once('./include/header.php') ?>
+
+<!-- Banner -->
+<section class="inner-banner risk" style="background-image: url(images/risk.jpeg);">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+					Refund Policy
+               </h2>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Banner -->
+
+<section class="refund-policy">
+   <div class="container">
+      <div class="col-md-12">
+         <p>
+            The Company may go for an exception in some cases when it is necessary to return payment made by the credit card, but only if the Client justifies the reason of doing this. The Client may submit a request to refund in case service not provided or not as described, the service received is not functioning appropriately. A refund is possible in case when the account had been deposited, but no trades were done and is made only on the credit card that was used for depositing.
+         </p>
+         <p>
+            In order to start the return process the Client shall submit a cancellation request by emailing to finance@nxgmarkets.com. A cancellation request shall contain the following information at least but not limited to:
+         </p>
+         <ol>
+            <li>
+               Full name of the Client;
+            </li>
+            <li>
+               Residential address;
+            </li>
+            <li>
+               Contact e-mail and phone number(-s);
+            </li>
+            <li>
+               Trading account number(-s);
+            </li>
+            <li>
+               Initial payment amount, date of payment, used payment method(i.e. credit/debit cards);
+            </li>
+            <li>
+               Payment identification number(if any);
+            </li>
+            <li>
+               The reasons of the cancellation subject to the conditions stated below.
+            </li>
+         </ol>
+         <p>
+            All the information in a cancellation request submitted to the Company shall be identical to that originally submitted in the initial payment.
+         </p>
+         <p>
+            All received cancellation requests to be dealt with by the Company on the following terms and conditions:
+         </p>
+         <ol>
+            <li>
+               All cancellation requests shall be for genuine and acceptable reasons, and those reasons shall be described in detail by the Client in the cancellation request;
+            </li>
+            <li>
+               All cancellation requests shall be submitted within 7 days from the moment of the initial payment; the Company shall notify the Client in cases where it is not possible to process the cancellation request due to card scheme or payment institution rules;
+            </li>
+            <li>
+               All cancellation requests shall be processed within 2 business days as per term set by the Client Agreement for claims related to non-trading operations or exceptionally more; the Company shall notify the Client in cases when additional time is required;
+            </li>
+            <li>
+               All cancellation requests shall be reviewed only if the requested amount does not exceed initial payment amount and as well as free margin of trading account;
+            </li>
+            <li>
+               Any charges deducted by the payment institution and also any loss or expense, if suffered or incurred by the Company due to adverse exchange rate fluctuation to be covered from the return amount.
+            </li>
+         </ol>
+      </div>
+   </div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
