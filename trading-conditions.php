87850fc3f0cd3c5acf8496ba45de78845db0a2bf
amitfxcareers <amit@fxcareers.com>
2024-06-13 12:05:30 +0530

    Trading Conditions Page Done

--- trading-conditions.php
+++ trading-conditions.php
@@ -19,6 +19,94 @@
 </section>
 <!-- Banner -->
 
+<!-- Trading Platform -->
+<section class="trusted-trading-platform">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<p>
+					At NXG Markets, we provide an outstanding trading setting to enhance your strategies. Benefit from competitive spreads to maximize profits and access a variety of instruments for a well-rounded portfolio. Enjoy the best in trading conditions.
+				</p>
+				<h5 class="fw-600">
+					Welcome to NXG Markets
+				</h5>
+				<h2 class="text-primary fw-800">
+					A Trusted Trading Platform
+				</h2>
+				<p>
+					Discover trust at NXG Markets, where we offer a secure and reliable trading environment, ensuring a seamless and rewarding experience. Join today to access trading success with our comprehensive tools and competitive conditions. Don’t miss out – register now!
+				</p>
+				<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+			</div>
+			<div class="col-md-6">
+				<lottie-player src="images/lottie-json/trading-platform.json" style="width: 100%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Trading Platform -->
+
+<!-- Key Trading Principles -->
+<section class="key-trading-principles">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h5 class="text-white fw-600">
+					Trading Environment
+				</h5>
+				<h2 class="text-white fw-800">
+					Key Trading Principles
+				</h2>
+			</div>
+		</div>
+		<div class="row mt-4 align-items-center">
+			<div class="col-md-6">
+				<div class="key-principles">
+					<p>
+						<span>1</span>
+						Embrace potential losses as an inherent part of trading.
+					</p>
+					<p>
+						<span>2</span>
+						Take full responsibility for your trading decisions.
+					</p>
+					<p>
+						<span>3</span>
+						Abandon unrealistic hopes; exit worsening market positions promptly.
+					</p>
+					<p>
+						<span>4</span>
+						Trade with a well-thought-out plan, balancing risk and reward.
+					</p>
+					<p>
+						<span>5</span>
+						Avoid greed; stick to your set goals and withdraw profits timely.
+					</p>
+					<p>
+						<span>6</span>
+						Keep emotions in check; adhere to your plan and set stop losses.
+					</p>
+					<p>
+						<span>7</span>
+						Overcome market fear for substantial investment growth.
+					</p>
+					<p>
+						<span>8</span>
+						Leverage news-driven market movements for short, rapid gains.
+					</p>
+					<p>
+						<span>9</span>
+						Align with the trend for increased profits.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<lottie-player src="images/lottie-json/key-principles.json" style="width: 100%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Key Trading Principles -->
 
 <?php include_once('./include/why-nxg-markets.php') ?>
 <?php include_once('./include/multi-language-assistance.php') ?>
