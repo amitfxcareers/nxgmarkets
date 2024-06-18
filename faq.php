d73f080e60e901ab6d4d2ab8982655302f27ca23
amitfxcareers <amit@fxcareers.com>
2024-06-18 16:36:54 +0530

    FAQ Page created and content updated

---
+++ faq.php
@@ -0,0 +1,217 @@
+<?php include_once('./include/header.php') ?>
+<!-- Banner -->
+<section class="inner-banner" style="background-image: url(images/funding_options_banner.webp);">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  Frequently Asked Questions
+               </h2>
+               <p class="text-white">
+                  <!-- Fast, Secure and Easy Deposit And Withdrawal Options. -->
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register Now</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Banner -->
+<!-- FAQs -->
+<section class="faq-section">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="faq-listing">
+               <div class="accordion faq-accordion" id="accordionExample">
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Who can Become Master Strategy Provider?
+                        </button>
+                     </h2>
+                     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>Any one can Become Master and offer strategy.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                        What Is External Account?
+                        </button>
+                     </h2>
+                     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>External account is the account on which the manager trades. The account balance must be 0 when opened.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                        What is Reward Account?
+                        </button>
+                     </h2>
+                     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>A trading account to charge rewards.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                        Can we make Single Account for Reward and Trading?
+                        </button>
+                     </h2>
+                     <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>You cannot specify as External Account an account that is already specified as Reward Account.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                        What is Rollovers?
+                        </button>
+                     </h2>
+                     <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>Rollovers is the period during which you will be able to make transactions with investments (open or close an investment, withdraw or deposit funds). During this period, Copy Trader Invest should process the accumulated requests and execute incoming requests immediately.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+                        What is the correlation between the Enable and Disable settings?
+                        </button>
+                     </h2>
+                     <div id="collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>
+                              <b>Disable</b><br>
+                              Investment-related requests are processed automatically, without waiting for confirmation
+                              <br>
+                              <b>Enable</b><br>
+                              Investment-related requests are processed automatically, but only during the time period set in Rollovers days and Rollovers start/end time.
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+                        How do I get credentials for Master Trading Account ?
+                        </button>
+                     </h2>
+                     <div id="collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>After creating a strategy, a trading account will be automatically created . The message with credentials (server, login, password and reward account) will be sent by mail and as a notification in the Client Interface.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="true" aria-controls="collapseEight">
+                        Is Investor allow deposit and withdrawal?
+                        </button>
+                     </h2>
+                     <div id="collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>Yes, You can deposit or withdraw money from your investments.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="true" aria-controls="collapseEight">
+                        How to view information and settings for manager Strategy?
+                        </button>
+                     </h2>
+                     <div id="collapseNine" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>The list of strategies and their details can also be found in Client Interface → Invest Rating. Here you can see the reward frequency, reward amount, type of reward, rollover settings as well as the yield chart.</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="accordion-item border-0 mb-2">
+                     <h2 class="accordion-header">
+                        <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="true" aria-controls="collapseEight">
+                        How do I exit the Strategy ?
+                        </button>
+                     </h2>
+                     <div id="collapseTen" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           <p>To close the investment, navigate to Client Interface → Accounts → Investments, select the investment and click Deactivate (the cross to the right of the strategy name).<br>
+                           If automatic deactivation was not enabled, wait for the Manager to approve the deactivation.<br>
+                           Closed strategies are displayed in the Closed section</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- FAQs -->
+<!-- Why Choose NXG Markets for Trading? -->
+<section class="why-trade-with-nxg">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <div class="mb-5">
+               <h2 class="text-white">
+                  Why Choose NXG Markets for Trading?
+               </h2>
+            </div>
+            <div class="icon-box">
+               <div class="icon">
+                  <img src="images/competitive-trading-conditions.png" class="img-fluid">
+               </div>
+               <h4 class="title">Best Trading Conditions</h4>
+               <p class="description">Experience the advantages of narrow spreads, minimal commissions, and rapid order execution at the sub-second level. We prioritize competitive pricing to enhance the effectiveness of your trading strategies.</p>
+            </div>
+            <div class="icon-box">
+               <div class="icon">
+                  <img src="images/wide-range-of-currency-pairs.png" class="img-fluid">
+               </div>
+               <h4 class="title">Range of Currency Pairs</h4>
+               <p class="description">Trade over 40 currency pairs, including major global currencies with high liquidity. Expand your portfolio and capitalize on opportunities arising from various market movements.</p>
+            </div>
+            <div class="icon-box">
+               <div class="icon">
+                  <img src="images/advanced-trading-platforms.png" class="img-fluid">
+               </div>
+               <h4 class="title">Advanced Trading Platforms</h4>
+               <p class="description">Opt for MT5, or cTrader. Effortlessly trade on the platform that aligns with your style and preferences.</p>
+            </div>
+            <div class="icon-box">
+               <div class="icon">
+                  <img src="images/secure-and-convenient-transactions.png" class="img-fluid">
+               </div>
+               <h4 class="title">Secure and Seamless Transactions</h4>
+               <p class="description">Benefit from a variety of deposit and withdrawal options, ensuring secure and straightforward fund management.</p>
+            </div>
+         </div>
+         <div class="col-md-6 text-center">
+            <img src="images/why-trade-forex.png" class="img-fluid" width="80%">
+         </div>
+      </div>
+      <div class="row mt-4">
+         <div class="col-md-12">
+            <div class="text-center">
+               <?php include_once('./include/register-now-button.php') ?>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Why Choose NXG Markets for Trading? -->
+<?php include_once('./include/why-nxg-markets.php') ?>
+<?php include_once('./include/footer.php') ?>
