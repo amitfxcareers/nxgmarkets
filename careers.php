d9b1a94e496b67d41b7122ac952b535fe211b869
amitfxcareers <amit@fxcareers.com>
2024-07-08 15:26:56 +0530

    Careers Page done

--- careers.php
+++ careers.php
@@ -1,23 +1,150 @@
 <?php include_once('./include/header.php') ?>
 <!-- Banner -->
 <section class="inner-banner" style="background-image: url(images/advantages-banner.png);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="banner_content ">
-					<h2 class="text-white">
-						Job Opportunities
-					</h2>
-					<p class="text-white">
-						Become a part of NXG Markets: Revolutionize Forex, Transform Your Career.
-					</p>
-					<a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="banner_content ">
+               <h2 class="text-white">
+                  Job Opportunities
+               </h2>
+               <p class="text-white">
+                  Become a part of NXG Markets: Revolutionize Forex, Transform Your Career.
+               </p>
+               <a href="https://my.nxgmarkets.com/auth/register" class="theme-bttn"><i class="fa fa-user" aria-hidden="true"></i>Register</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </section>
 <!-- Banner -->
-
+<!-- Careers Intro -->
+<section class="careers-intro">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <p>
+               Start an exciting career with NXG Markets, a frontrunner in the financial industry. We’re seeking passionate individuals ready to excel in trading, technology, and customer service. Join our dynamic team and open doors to limitless opportunities for growth, learning, and professional evolution.
+            </p>
+            <h5 class="fw-600">
+               Leading Employer
+            </h5>
+            <h2 class="text-primary fw-800">
+               Careers at NXG Markets
+            </h2>
+            <p>
+               Join NXG Markets, a top employer in the financial industry. We’re looking for talented individuals passionate about trading, technology, and customer service. In our supportive and collaborative work environment, discover opportunities for growth, continuous learning, and professional development. If you’re driven, ambitious, and ready to make an impact, join our dynamic team at NXG Markets.
+            </p>
+         </div>
+         <div class="col-md-6">
+            <lottie-player src="images/lottie-json/careers.json" style="width: 80%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-md-12">
+            <p>
+               Check out our current job openings and take the next step in your career today.
+            </p>
+         </div>
+      </div>
+      <div class="row mt-4">
+         <div class="col-md-12">
+            <table class="careers-table">
+               <thead>
+                  <tr>
+                     <th>
+                        <span>Position</span>
+                     </th>
+                     <th>
+                        <span>Location</span>
+                     </th>
+                     <th>
+                        <span>Term</span>
+                     </th>
+                     <th>
+                        <span>Apply Now</span>
+                     </th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr>
+                     <td>
+                        <span>Australian Dollar VS Canadian Dollar</span>
+                     </td>
+                     <td>
+                        <span>AUD 100 000</span>
+                     </td>
+                     <td>
+                        <span>Full-time</span>
+                     </td>
+                     <td>
+                        <span>
+                        	<a href="careers.php" class="theme-btn theme-btn2">
+                        		Apply Now
+                        	</a>
+                        </span>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <span>Australian Dollar VS Canadian Dollar</span>
+                     </td>
+                     <td>
+                        <span>AUD 100 000</span>
+                     </td>
+                     <td>
+                        <span>Full-time</span>
+                     </td>
+                     <td>
+                        <span>
+                        	<a href="careers.php" class="theme-btn theme-btn2">
+                        		Apply Now
+                        	</a>
+                        </span>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <span>Australian Dollar VS Canadian Dollar</span>
+                     </td>
+                     <td>
+                        <span>AUD 100 000</span>
+                     </td>
+                     <td>
+                        <span>Full-time</span>
+                     </td>
+                     <td>
+                        <span>
+                        	<a href="careers.php" class="theme-btn theme-btn2">
+                        		Apply Now
+                        	</a>
+                        </span>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <span>Australian Dollar VS Canadian Dollar</span>
+                     </td>
+                     <td>
+                        <span>AUD 100 000</span>
+                     </td>
+                     <td>
+                        <span>Full-time</span>
+                     </td>
+                     <td>
+                        <span>
+                        	<a href="careers.php" class="theme-btn theme-btn2">
+                        		Apply Now
+                        	</a>
+                        </span>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Careers Intro -->
 <?php include_once('./include/multi-language-assistance.php') ?>
 <?php include_once('./include/footer.php') ?>
